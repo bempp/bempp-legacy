0d92d8c73af4a3823a55a9d3370ecee445e9eaaa
Timo Betcke <timo.betcke@gmail.com>
2011-11-21 22:45:25 +0100

    Updated cmake files

--- cmake/BemppFindDependencies.cmake
+++ cmake/BemppFindDependencies.cmake
@@ -2,5 +2,5 @@
 find_package( Boost REQUIRED)
 
 # BLAS
-find_package( BLAS REQUIRED)
+set( BLAS_LIBRARIES "" CACHE STRING "Space separated list of full paths to BLAS libs")
 
--- cmake/BemppOptions.cmake
+++ cmake/BemppOptions.cmake
@@ -1,3 +1,4 @@
 # Options (can be modified by user)
 option(WITH_TESTS "Compile unit tests (can be run with 'make test')" ON)
 
+set(DUNE_PATH ${CMAKE_SOURCE_DIR}/contrib/dune CACHE PATH "Path to Dune")
