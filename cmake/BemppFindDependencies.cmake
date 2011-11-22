41c09d47d37541f5f46ed82888927307e098c76c
wsmigaj <w.smigaj@ucl.ac.uk>
2011-11-22 14:06:02 +0000

    Moved Dune dependency search to BemppFindDependencies.cmake.
    In this way not only the library, but also the examples and tests depend on Dune.

--- cmake/BemppFindDependencies.cmake
+++ cmake/BemppFindDependencies.cmake
@@ -4,3 +4,11 @@ find_package( Boost REQUIRED)
 # BLAS
 set( BLAS_LIBRARIES "" CACHE STRING "Space separated list of full paths to BLAS libs")
 
+# Dune
+
+find_library(LIB_DUNE_COMMON common ${CMAKE_SOURCE_DIR}/contrib/dune/dune-common/dune/common/.libs)
+find_library(LIB_DUNE_GRID grid ${CMAKE_SOURCE_DIR}/contrib/dune/dune-grid/dune/grid/.libs)
+
+include_directories(${CMAKE_SOURCE_DIR}/contrib/dune/dune-common)
+include_directories(${CMAKE_SOURCE_DIR}/contrib/dune/dune-grid)
+include_directories(${CMAKE_SOURCE_DIR}/contrib/dune/dune-foamgrid)
