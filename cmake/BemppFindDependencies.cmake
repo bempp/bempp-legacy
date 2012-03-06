407726ab970cad5ace2aeaca96da4801cdf14a83
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-03-06 11:10:18 +0000

    Improved dependency handling.

--- cmake/BemppFindDependencies.cmake
+++ cmake/BemppFindDependencies.cmake
@@ -1,17 +1,19 @@
 # Boost
-find_package( Boost REQUIRED)
+set(BOOST_LIBRARY_DIR "" CACHE PATH "Directory containing Boost Libraries")
+set(BOOST_INCLUDE_DIR "" CACHE PATH "Directory containing Boost include files")
+include_directories(${BOOST_INCLUDE_DIR})
 
 # BLAS
-set( BLAS_LIBRARIES "" CACHE STRING "Space separated list of full paths to BLAS libs")
+set(BLAS_LIBRARIES "" CACHE STRING "Space-separated list of full paths to BLAS libs")
+set(AHMED_INCLUDE_DIR "" CACHE PATH "Full path to the AHMED include directory")
+set(AHMED_LIBRARY "" CACHE FILEPATH "Full path to the AHMED library")
+set(METIS_LIBRARY "" CACHE FILEPATH "Full path to the METIS library")
 
 # Dune
-
 find_library(LIB_DUNE_COMMON common ${CMAKE_SOURCE_DIR}/contrib/dune/dune-common/dune/common/.libs)
 find_library(LIB_DUNE_GRID grid ${CMAKE_SOURCE_DIR}/contrib/dune/dune-grid/dune/grid/.libs)
-
 include_directories(${CMAKE_SOURCE_DIR}/contrib/dune/dune-common)
 include_directories(${CMAKE_SOURCE_DIR}/contrib/dune/dune-grid)
 include_directories(${CMAKE_SOURCE_DIR}/contrib/dune/dune-localfunctions)
 include_directories(${CMAKE_SOURCE_DIR}/contrib/dune/dune-foamgrid)
-include_directories(${CMAKE_SOURCE_DIR}/contrib/ahmed/Include)
 
--- cmake/BemppOptions.cmake
+++ cmake/BemppOptions.cmake
@@ -1,4 +1,5 @@
 # Options (can be modified by user)
 option(WITH_TESTS "Compile unit tests (can be run with 'make test')" ON)
+option(WITH_AHMED "Link to the AHMED library to enable ACA mode assembly)" ON)
 
 set(DUNE_PATH ${CMAKE_SOURCE_DIR}/contrib/dune CACHE PATH "Path to Dune")
