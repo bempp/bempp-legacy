4492165211b03c5ecab53e158800449f779b2fa4
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-03-21 10:59:42 +0000

    Implemented calculation of surface curl in 3D.

--- lib/fiber/scalar_space_mapping.hpp
+++ lib/fiber/scalar_space_mapping.hpp
@@ -1,6 +1,7 @@
 #ifndef fiber_scalar_space_mapping_hpp
 #define fiber_scalar_space_mapping_hpp
 
+#include <cassert>
 #include <stdexcept>
 #include "basis_data.hpp"
 #include "geometrical_data.hpp"
@@ -19,9 +20,8 @@ public:
     }
 
     static void addSurfaceCurlDependencies(int& basisDeps, int& geomDeps) {
-        throw std::logic_error("ScalarSpaceMapping::"
-                               "addSurfaceCurlDependencies(): "
-                               "Not implemented yet");
+        basisDeps |= DERIVATIVES;
+        geomDeps |= NORMALS | JACOBIAN_INVERSES_TRANSPOSED;
     }
 
     static void evaluateShapeFunctions(const BasisData<ValueType>& basisData,
@@ -30,13 +30,42 @@ public:
         result = basisData.values;
     }
 
-    static void evaluateSurfaceCurls(const BasisData<ValueType>& basisData,
+    /**
+      \param[out] result
+        dimensions: (worldDim, functionCount, pointCount)
+    */
+    static void evaluateSurfaceCurls3D(const BasisData<ValueType>& basisData,
                                      const GeometricalData<ValueType>& geomData,
-                                     arma::Mat<ValueType>& result) {
-        throw std::logic_error("ScalarSpaceMapping::"
-                               "evaluateSurfaceCurls(): "
-                               "Not implemented yet");
+                                     arma::Cube<ValueType>& result) {
+        const arma::Mat<ValueType>& n = geomData.normals;
+        // jt(i, j): dx_j/dq_i
+        const arma::Cube<ValueType>& jit = geomData.jacobianInversesTransposed;
+        const Array4D<ValueType>& d = basisData.derivatives;
+
+        assert(d.extent(0) == 1); // scalar functions
+        const int worldDim = 3;
+        assert(d.extent(1) + 1 == worldDim);
+        const int functionCount = d.extent(2);
+        const int pointCount = d.extent(3);
+
+        result.set_size(worldDim, functionCount, pointCount);
+
+        for (int p = 0; p < pointCount; ++p)
+            for (int f = 0; f < functionCount; ++f) {
+                // vec := gradient of the basis function extended outside
+                // the surface so that its normal derivative on the surf. is zero
+                arma::Col<ValueType> vec(3);
+                vec(0) = d(0, 0, f, p) * jit(0, 0, p) + d(0, 1, f, p) * jit(0, 1, p);
+                vec(1) = d(0, 0, f, p) * jit(1, 0, p) + d(0, 1, f, p) * jit(1, 1, p);
+                vec(2) = d(0, 0, f, p) * jit(2, 0, p) + d(0, 1, f, p) * jit(2, 1, p);
+
+                // result := n \times vec
+                result(0, f, p) = n(1, p) * vec(2) - n(2, p) * vec(1);
+                result(1, f, p) = n(2, p) * vec(0) - n(0, p) * vec(2);
+                result(2, f, p) = n(0, p) * vec(1) - n(1, p) * vec(0);
+            }
     }
+
 };
 
 } // namespace Fiber
---
+++ lib/fiber/surface_curl_3d.hpp
@@ -0,0 +1,59 @@
+// Copyright (C) 2011-2012 by the BEM++ Authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef fiber_surface_curl_3d_hpp
+#define fiber_surface_curl_3d_hpp
+
+#include "expression.hpp"
+#include "scalar_space_mapping.hpp"
+
+#include <armadillo>
+#include <vector>
+
+namespace Fiber {
+
+template <typename ValueType>
+class SurfaceCurl3D : public Expression<ValueType>
+{
+public:
+    virtual int domainDimension() const {
+        return 1;
+    }
+
+    virtual int codomainDimension() const {
+        return 3;
+    }
+
+    virtual void addDependencies(int& basisDeps, int& geomDeps) const {
+        ScalarSpaceMapping<ValueType>::
+                addSurfaceCurlDependencies(basisDeps, geomDeps);
+    }
+
+    virtual void evaluate(const BasisData<ValueType>& basisData,
+                          const GeometricalData<ValueType>& geomData,
+                          arma::Cube<ValueType>& result) const {
+        ScalarSpaceMapping<ValueType>::
+                evaluateSurfaceCurls3D(basisData, geomData, result);
+    }
+};
+
+} // namespace Fiber
+
+#endif
