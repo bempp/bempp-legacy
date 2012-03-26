9b850056777979086de4e6aa53e2f1a83d5735a1
Joel Phillips <joel.m.phillips@gmail.com>
2012-03-26 13:22:58 +0100

    Ensure that OpenCL builds do use OpenCL

--- examples/assembly.cpp
+++ examples/assembly.cpp
@@ -45,6 +45,7 @@
 
 #include "space/piecewise_linear_continuous_scalar_space.hpp"
 
+
 #include <cmath>
 
 using namespace Bempp;
@@ -89,188 +90,198 @@ inline bool approxZero(double x)
   */
 int main()
 {
-    const MeshVariant meshVariant = CUBE_12_REORIENTED;
-    const OperatorVariant opVariant = DOUBLE_LAYER_POTENTIAL;
+	try {
+		const MeshVariant meshVariant = CUBE_384;
+		const OperatorVariant opVariant = DOUBLE_LAYER_POTENTIAL;
 
-    const char TWO_DISJOINT_TRIANGLES_FNAME[] = "two_disjoint_triangles.msh";
-    const char TWO_TRIANGLES_SHARING_VERTEX_0_FNAME[] = "two_triangles_sharing_vertex_0.msh";
-    const char TWO_TRIANGLES_SHARING_VERTICES_2_AND_0_FNAME[] = "two_triangles_sharing_vertices_2_and_0.msh";
-    const char TWO_TRIANGLES_SHARING_VERTICES_1_AND_0_FNAME[] = "two_triangles_sharing_vertices_1_and_0.msh";
-    const char TWO_TRIANGLES_SHARING_EDGES_0_AND_0_FNAME[] = "two_triangles_sharing_edges_0_and_0.msh";
-    const char TWO_TRIANGLES_SHARING_EDGES_1_AND_0_FNAME[] = "two_triangles_sharing_edges_1_and_0.msh";
-    const char SIMPLE_MESH_9_FNAME[] = "simple_mesh_9_elements.msh";
-    const char CUBE_12_FNAME[] = "cube-12.msh";
-    const char CUBE_12_REORIENTED_FNAME[] = "cube-12-reoriented.msh";
-    const char CUBE_384_FNAME[] = "cube-384.msh";
+		const char TWO_DISJOINT_TRIANGLES_FNAME[] = "two_disjoint_triangles.msh";
+		const char TWO_TRIANGLES_SHARING_VERTEX_0_FNAME[] = "two_triangles_sharing_vertex_0.msh";
+		const char TWO_TRIANGLES_SHARING_VERTICES_2_AND_0_FNAME[] = "two_triangles_sharing_vertices_2_and_0.msh";
+		const char TWO_TRIANGLES_SHARING_VERTICES_1_AND_0_FNAME[] = "two_triangles_sharing_vertices_1_and_0.msh";
+		const char TWO_TRIANGLES_SHARING_EDGES_0_AND_0_FNAME[] = "two_triangles_sharing_edges_0_and_0.msh";
+		const char TWO_TRIANGLES_SHARING_EDGES_1_AND_0_FNAME[] = "two_triangles_sharing_edges_1_and_0.msh";
+		const char SIMPLE_MESH_9_FNAME[] = "simple_mesh_9_elements.msh";
+		const char CUBE_12_FNAME[] = "cube-12.msh";
+		const char CUBE_12_REORIENTED_FNAME[] = "cube-12-reoriented.msh";
+		const char CUBE_384_FNAME[] = "cube-384.msh";
 
-    const char* MESH_FNAME = 0;
-    switch (meshVariant) {
-    case TWO_DISJOINT_TRIANGLES:
-        MESH_FNAME = TWO_DISJOINT_TRIANGLES_FNAME; break;
-    case TWO_TRIANGLES_SHARING_VERTEX_0:
-        MESH_FNAME = TWO_TRIANGLES_SHARING_VERTEX_0_FNAME; break;
-    case TWO_TRIANGLES_SHARING_VERTICES_2_AND_0:
-        MESH_FNAME = TWO_TRIANGLES_SHARING_VERTICES_2_AND_0_FNAME; break;
-    case TWO_TRIANGLES_SHARING_VERTICES_1_AND_0:
-        MESH_FNAME = TWO_TRIANGLES_SHARING_VERTICES_1_AND_0_FNAME; break;
-    case TWO_TRIANGLES_SHARING_EDGES_0_AND_0:
-        MESH_FNAME = TWO_TRIANGLES_SHARING_EDGES_0_AND_0_FNAME; break;
-    case TWO_TRIANGLES_SHARING_EDGES_1_AND_0:
-        MESH_FNAME = TWO_TRIANGLES_SHARING_EDGES_1_AND_0_FNAME; break;
-    case SIMPLE_MESH_9:
-        MESH_FNAME = SIMPLE_MESH_9_FNAME; break;
-    case CUBE_12:
-        MESH_FNAME = CUBE_12_FNAME; break;
-    case CUBE_12_REORIENTED:
-        MESH_FNAME = CUBE_12_REORIENTED_FNAME; break;
-    case CUBE_384:
-        MESH_FNAME = CUBE_384_FNAME; break;
-    default:
-        throw std::runtime_error("Invalid mesh name");
-    }
+		const char* MESH_FNAME = 0;
+		switch (meshVariant) {
+		case TWO_DISJOINT_TRIANGLES:
+			MESH_FNAME = TWO_DISJOINT_TRIANGLES_FNAME; break;
+		case TWO_TRIANGLES_SHARING_VERTEX_0:
+			MESH_FNAME = TWO_TRIANGLES_SHARING_VERTEX_0_FNAME; break;
+		case TWO_TRIANGLES_SHARING_VERTICES_2_AND_0:
+			MESH_FNAME = TWO_TRIANGLES_SHARING_VERTICES_2_AND_0_FNAME; break;
+		case TWO_TRIANGLES_SHARING_VERTICES_1_AND_0:
+			MESH_FNAME = TWO_TRIANGLES_SHARING_VERTICES_1_AND_0_FNAME; break;
+		case TWO_TRIANGLES_SHARING_EDGES_0_AND_0:
+			MESH_FNAME = TWO_TRIANGLES_SHARING_EDGES_0_AND_0_FNAME; break;
+		case TWO_TRIANGLES_SHARING_EDGES_1_AND_0:
+			MESH_FNAME = TWO_TRIANGLES_SHARING_EDGES_1_AND_0_FNAME; break;
+		case SIMPLE_MESH_9:
+			MESH_FNAME = SIMPLE_MESH_9_FNAME; break;
+		case CUBE_12:
+			MESH_FNAME = CUBE_12_FNAME; break;
+		case CUBE_12_REORIENTED:
+			MESH_FNAME = CUBE_12_REORIENTED_FNAME; break;
+		case CUBE_384:
+			MESH_FNAME = CUBE_384_FNAME; break;
+		default:
+			throw std::runtime_error("Invalid mesh name");
+		}
 
-    // Import the grid
-    GridParameters params;
-    params.topology = GridParameters::TRIANGULAR;
+		// Import the grid
+		GridParameters params;
+		params.topology = GridParameters::TRIANGULAR;
 
-    std::auto_ptr<Grid> grid(GridFactory::importGmshGrid(params, std::string(MESH_FNAME),
-                             true, // verbose
-                             false)); // insertBoundarySegments
+		std::auto_ptr<Grid> grid(GridFactory::importGmshGrid(params, std::string(MESH_FNAME),
+								 true, // verbose
+								 false)); // insertBoundarySegments
 
-    std::cout << "Elements:\n";
-    std::auto_ptr<GridView> view = grid->leafView();
-    const Mapper& elementMapper = view->elementMapper();
-    std::auto_ptr<EntityIterator<0> > it = view->entityIterator<0>();
-    while (!it->finished())
-    {
-        const Entity<0>& entity = it->entity();
-        arma::Mat<double> corners;
-        entity.geometry().corners(corners);
-        std::cout << "Element #" << elementMapper.entityIndex(entity) << ":\n";
-        std::cout << corners << '\n';
-        it->next();
-    }
-    std::cout.flush();
+		std::cout << "Elements:\n";
+		std::auto_ptr<GridView> view = grid->leafView();
+		const Mapper& elementMapper = view->elementMapper();
+		std::auto_ptr<EntityIterator<0> > it = view->entityIterator<0>();
+		while (!it->finished())
+		{
+			const Entity<0>& entity = it->entity();
+			arma::Mat<double> corners;
+			entity.geometry().corners(corners);
+			std::cout << "Element #" << elementMapper.entityIndex(entity) << ":\n";
+			std::cout << corners << '\n';
+			it->next();
+		}
+		std::cout.flush();
 
-    PiecewiseLinearContinuousScalarSpace<double> space(*grid);
-    space.assignDofs();
+		PiecewiseLinearContinuousScalarSpace<double> space(*grid);
+		space.assignDofs();
 
-    AssemblyOptions assemblyOptions;
-//    assemblyOptions.switchToDense();
+		AssemblyOptions assemblyOptions;
+		assemblyOptions.switchToDense();
 
-    AcaOptions acaOptions;
-    acaOptions.eps = 1e-4;
-    acaOptions.maximumRank = 10000;
-    acaOptions.minimumBlockSize = 2;
-    acaOptions.eta = 0.8;
-    acaOptions.recompress = true;
-    assemblyOptions.switchToAca(acaOptions);
+		AcaOptions acaOptions;
+		acaOptions.eps = 1e-4;
+		acaOptions.maximumRank = 10000;
+		acaOptions.minimumBlockSize = 2;
+		acaOptions.eta = 0.8;
+		acaOptions.recompress = true;
+	//	assemblyOptions.switchToAca(acaOptions);
 
-//    assemblyOptions.switchToSparse();
+	//    assemblyOptions.switchToSparse();
 
-    assemblyOptions.switchToTbbAndOpenMp();
-    assemblyOptions.setSingularIntegralCaching(AssemblyOptions::NO);
+	//    assemblyOptions.switchToTbbAndOpenMp();
+	//	assemblyOptions.setSingularIntegralCaching(AssemblyOptions::YES);
 
-    Fiber::StandardLocalAssemblerFactoryForOperatorsOnSurfaces<double, GeometryFactory>
-            factory;
 
-    typedef std::auto_ptr<LinearOperator<double> > LinearOperatorPtr;
-    LinearOperatorPtr op;
-    switch (opVariant)
-    {
-    case SINGLE_LAYER_POTENTIAL:
-        op = LinearOperatorPtr(new SingleLayerPotential3D<double>); break;
-    case DOUBLE_LAYER_POTENTIAL:
-        op = LinearOperatorPtr(new DoubleLayerPotential3D<double>); break;
-    case ADJOINT_DOUBLE_LAYER_POTENTIAL:
-        op = LinearOperatorPtr(new AdjointDoubleLayerPotential3D<double>); break;
-    case HYPERSINGULAR_OPERATOR:
-        op = LinearOperatorPtr(new HypersingularOperator3D<double>); break;
-    case IDENTITY:
-        op = LinearOperatorPtr(new IdentityOperator<double>); break;
-    default:
-        throw std::runtime_error("Invalid operator");
-    }
+		Fiber::OpenClOptions openCLopts;
+		assemblyOptions.switchToOpenCl(openCLopts);
 
-    std::auto_ptr<DiscreteScalarValuedLinearOperator<double> > result =
-            op->assembleWeakForm(space, space, factory, assemblyOptions);
+		Fiber::StandardLocalAssemblerFactoryForOperatorsOnSurfaces<double, GeometryFactory>
+				factory;
 
-    arma::Mat<double> resultMatrix = result->asMatrix();
-    std::cout << "\nGenerated matrix:\n" << resultMatrix << std::endl;
+		typedef std::auto_ptr<LinearOperator<double> > LinearOperatorPtr;
+		LinearOperatorPtr op;
+		switch (opVariant)
+		{
+		case SINGLE_LAYER_POTENTIAL:
+			op = LinearOperatorPtr(new SingleLayerPotential3D<double>); break;
+		case DOUBLE_LAYER_POTENTIAL:
+			op = LinearOperatorPtr(new DoubleLayerPotential3D<double>); break;
+		case ADJOINT_DOUBLE_LAYER_POTENTIAL:
+			op = LinearOperatorPtr(new AdjointDoubleLayerPotential3D<double>); break;
+		case HYPERSINGULAR_OPERATOR:
+			op = LinearOperatorPtr(new HypersingularOperator3D<double>); break;
+		case IDENTITY:
+			op = LinearOperatorPtr(new IdentityOperator<double>); break;
+		default:
+			throw std::runtime_error("Invalid operator");
+		}
 
-    if (opVariant == SINGLE_LAYER_POTENTIAL)
-    {
-        if (meshVariant == TWO_DISJOINT_TRIANGLES)
-        {
-            // disjoint elements
-            assert(approxEqual(resultMatrix(0, 3), 0.0101397));
-            assert(approxEqual(resultMatrix(0, 4), 0.00852427));
-            assert(approxEqual(resultMatrix(1, 3), 0.0127044));
-            // TODO: calculate with reasonable accuracy the result for coincident triangles
-            // and make a test
-        }
-        else if (meshVariant == TWO_TRIANGLES_SHARING_VERTEX_0)
-        {
-            // elements sharing vertex
-            assert(approxEqual(resultMatrix(1, 3), 0.0109119));
-            assert(approxEqual(resultMatrix(2, 4), 0.0123149));
-        }
-        else if (meshVariant == TWO_TRIANGLES_SHARING_VERTICES_2_AND_0)
-        {
-            // elements sharing vertex
-            assert(approxEqual(resultMatrix(0, 3), 0.0109119));
-            assert(approxEqual(resultMatrix(1, 4), 0.0123149));
-        }
-        else if (meshVariant == TWO_TRIANGLES_SHARING_VERTICES_1_AND_0)
-        {
-            // elements sharing vertex
-            assert(approxEqual(resultMatrix(0, 3), 0.0109119));
-            assert(approxEqual(resultMatrix(2, 4), 0.0123149));
-        }
-        else if (meshVariant == TWO_TRIANGLES_SHARING_EDGES_0_AND_0)
-        {
-            // elements sharing edge
-            assert(approxEqual(resultMatrix(2, 3), 0.00861375));
-        }
-        else if (meshVariant == TWO_TRIANGLES_SHARING_EDGES_1_AND_0)
-        {
-            // elements sharing edge
-            assert(approxEqual(resultMatrix(0, 3), 0.00861375));
-        }
-    }
-    else if (opVariant == HYPERSINGULAR_OPERATOR)
-    {
-        if (meshVariant == TWO_DISJOINT_TRIANGLES)
-        {
-            // disjoint elements
-            assert(approxEqual(resultMatrix(0, 3), 0.0912808 * 7. / 18.));
-            assert(approxEqual(resultMatrix(0, 4), 0.0912808 / 9.));
-            assert(approxEqual(resultMatrix(0, 5), 0.0912808 / -2.));
-            assert(approxEqual(resultMatrix(1, 4), 0.0912808 / -9.));
-            assert(approxZero(resultMatrix(1, 5)));
-        }
-    }
-    else if (opVariant == IDENTITY)
-    {
-        if (meshVariant == TWO_DISJOINT_TRIANGLES)
-        {
-            // disjoint elements
-            assert(approxEqual(resultMatrix(0, 0), 0.25));
-            assert(approxEqual(resultMatrix(0, 1), 0.125));
-            assert(approxEqual(resultMatrix(0, 2), 0.125));
-            assert(approxEqual(resultMatrix(4, 4), 0.5));
-            assert(approxEqual(resultMatrix(4, 5), 0.25));
-        }
-        else if (meshVariant == TWO_TRIANGLES_SHARING_VERTEX_0)
-        {
-            // elements sharing vertex
-            assert(approxEqual(resultMatrix(0, 0), 0.75));
-            assert(approxEqual(resultMatrix(0, 1), 0.125));
-            assert(approxEqual(resultMatrix(0, 2), 0.125));
-            assert(approxEqual(resultMatrix(0, 3), 0.25));
-            assert(approxEqual(resultMatrix(0, 4), 0.25));
-        }
-    }
+		std::auto_ptr<DiscreteScalarValuedLinearOperator<double> > result =
+				op->assembleWeakForm(space, space, factory, assemblyOptions);
+
+		arma::Mat<double> resultMatrix = result->asMatrix();
+//		std::cout << "\nGenerated matrix:\n" << resultMatrix << std::endl;
+
+		if (opVariant == SINGLE_LAYER_POTENTIAL)
+		{
+			if (meshVariant == TWO_DISJOINT_TRIANGLES)
+			{
+				// disjoint elements
+				assert(approxEqual(resultMatrix(0, 3), 0.0101397));
+				assert(approxEqual(resultMatrix(0, 4), 0.00852427));
+				assert(approxEqual(resultMatrix(1, 3), 0.0127044));
+				// TODO: calculate with reasonable accuracy the result for coincident triangles
+				// and make a test
+			}
+			else if (meshVariant == TWO_TRIANGLES_SHARING_VERTEX_0)
+			{
+				// elements sharing vertex
+				assert(approxEqual(resultMatrix(1, 3), 0.0109119));
+				assert(approxEqual(resultMatrix(2, 4), 0.0123149));
+			}
+			else if (meshVariant == TWO_TRIANGLES_SHARING_VERTICES_2_AND_0)
+			{
+				// elements sharing vertex
+				assert(approxEqual(resultMatrix(0, 3), 0.0109119));
+				assert(approxEqual(resultMatrix(1, 4), 0.0123149));
+			}
+			else if (meshVariant == TWO_TRIANGLES_SHARING_VERTICES_1_AND_0)
+			{
+				// elements sharing vertex
+				assert(approxEqual(resultMatrix(0, 3), 0.0109119));
+				assert(approxEqual(resultMatrix(2, 4), 0.0123149));
+			}
+			else if (meshVariant == TWO_TRIANGLES_SHARING_EDGES_0_AND_0)
+			{
+				// elements sharing edge
+				assert(approxEqual(resultMatrix(2, 3), 0.00861375));
+			}
+			else if (meshVariant == TWO_TRIANGLES_SHARING_EDGES_1_AND_0)
+			{
+				// elements sharing edge
+				assert(approxEqual(resultMatrix(0, 3), 0.00861375));
+			}
+		}
+		else if (opVariant == HYPERSINGULAR_OPERATOR)
+		{
+			if (meshVariant == TWO_DISJOINT_TRIANGLES)
+			{
+				// disjoint elements
+				assert(approxEqual(resultMatrix(0, 3), 0.0912808 * 7. / 18.));
+				assert(approxEqual(resultMatrix(0, 4), 0.0912808 / 9.));
+				assert(approxEqual(resultMatrix(0, 5), 0.0912808 / -2.));
+				assert(approxEqual(resultMatrix(1, 4), 0.0912808 / -9.));
+				assert(approxZero(resultMatrix(1, 5)));
+			}
+		}
+		else if (opVariant == IDENTITY)
+		{
+			if (meshVariant == TWO_DISJOINT_TRIANGLES)
+			{
+				// disjoint elements
+				assert(approxEqual(resultMatrix(0, 0), 0.25));
+				assert(approxEqual(resultMatrix(0, 1), 0.125));
+				assert(approxEqual(resultMatrix(0, 2), 0.125));
+				assert(approxEqual(resultMatrix(4, 4), 0.5));
+				assert(approxEqual(resultMatrix(4, 5), 0.25));
+			}
+			else if (meshVariant == TWO_TRIANGLES_SHARING_VERTEX_0)
+			{
+				// elements sharing vertex
+				assert(approxEqual(resultMatrix(0, 0), 0.75));
+				assert(approxEqual(resultMatrix(0, 1), 0.125));
+				assert(approxEqual(resultMatrix(0, 2), 0.125));
+				assert(approxEqual(resultMatrix(0, 3), 0.25));
+				assert(approxEqual(resultMatrix(0, 4), 0.25));
+			}
+		}
+	}
+	catch(const std::exception& e){
+		std::cerr<<"An exception was thrown"<<std::endl;
+		std::cerr<<e.what()<<std::endl;
+	}
 }
 
 
