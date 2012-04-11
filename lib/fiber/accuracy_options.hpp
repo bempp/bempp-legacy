f837da96cd166cf150bdcacca147fea814373cc4
wsmigaj <w.smigaj@ucl.ac.uk>
2012-04-11 22:50:56 +0100

    Extracted QuadratureOptions to its own file.

--- lib/fiber/accuracy_options.hpp
+++ lib/fiber/accuracy_options.hpp
@@ -1,31 +1,19 @@
 #ifndef fiber_accuracy_options_hpp
 #define fiber_accuracy_options_hpp
 
+#include "quadrature_options.hpp"
+
 namespace Fiber
 {
 
-struct QuadratureOptions
-{
-    QuadratureOptions() : mode(ORDER_INCREMENT), orderIncrement(0) {
-    }
-
-    enum Mode {
-        ORDER_INCREMENT, EXACT_ORDER
-    } mode;
-
-    union {
-        int orderIncrement;
-        int order;
-    };
-};
-
-class AccuracyOptions
+/** \brief Options controlling quadrature accuracy. */
+struct AccuracyOptions
 {
 public:
     QuadratureOptions regular;
     QuadratureOptions singular;
 };
 
-}
+} // namespace Fiber
 
 #endif
