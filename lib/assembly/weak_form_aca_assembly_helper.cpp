d8c4fb7036462e745d1b33754090d9c087c1a411
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-05-09 19:01:16 +0100

    Removed block dumping in WeakFormAcaAssemblyHelper.

--- lib/assembly/weak_form_aca_assembly_helper.cpp
+++ lib/assembly/weak_form_aca_assembly_helper.cpp
@@ -214,10 +214,6 @@ void WeakFormAcaAssemblyHelper<BasisFunctionType, ResultType>::cmpbl(
         m_sparseTermsToAdd[nTerm]->addBlock(
                     testGlobalDofs, trialGlobalDofs,
                     m_sparseTermsMultipliers[nTerm], result);
-
-     char buffer[1024];
-     sprintf(buffer, "block-%d-%d-%d-%d.txt", b1, n1, b2, n2);
-     arma::diskio::save_raw_ascii(result, buffer);
 }
 
 template <typename BasisFunctionType, typename ResultType>
