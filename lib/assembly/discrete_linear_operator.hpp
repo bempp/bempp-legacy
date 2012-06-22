cb9f6e87780233a5b5981d264b017651fa8ee8e8
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-06-22 16:33:10 +0100

    Added a sanity check to the built-in variant of DiscreteLinearOperator::apply().

--- lib/assembly/discrete_linear_operator.hpp
+++ lib/assembly/discrete_linear_operator.hpp
@@ -108,6 +108,14 @@ public:
                arma::Col<ValueType>& y_inout,
                const ValueType alpha,
                const ValueType beta) const {
+        bool transposed = (trans == TRANSPOSE || trans == CONJUGATE_TRANSPOSE);
+        if (x_in.n_rows != (transposed ? rowCount() : columnCount()))
+            throw std::invalid_argument("DiscreteLinearOperator::apply(): "
+                                        "vector x_in has invalid length");
+        if (y_inout.n_rows != (transposed ? columnCount() : rowCount()))
+            throw std::invalid_argument("DiscreteLinearOperator::apply(): "
+                                        "vector y_inout has invalid length");
+
         applyBuiltInImpl(trans, x_in, y_inout, alpha, beta);
     }
 
