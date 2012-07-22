a824ea7bd6a89b190ab8ebfe72e99fe8526386e1
Timo Betcke <timo.betcke@gmail.com>
2012-07-22 23:30:16 +0100

    Updated test_lapack.cpp

--- test_lapack/test_lapack.cpp
+++ test_lapack/test_lapack.cpp
@@ -1,14 +1,3 @@
-/*
-    AHMED -- Another software library on Hierarchical Matrices for
-             Elliptic Differential equations
-
-    Copyright (c) 2012 Mario Bebendorf
-
-    You should have received a copy of the license along with 
-    this software; if not, see AHMED's internet site.
-*/
-
-
 #include <complex>
 #include <iostream>
 
