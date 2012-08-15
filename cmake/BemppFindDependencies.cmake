cac3d438a7a7cd74c15aefa178e292b83a8ee430
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-08-15 16:35:15 +0100

    Changed types of some Cmake variables from STRING to PATH to enable path autocompletion in ccmake.

--- cmake/BemppFindDependencies.cmake
+++ cmake/BemppFindDependencies.cmake
@@ -1,25 +1,27 @@
 # Boost
-set(BOOST_INCLUDE_DIR "" CACHE STRING "Directory containing Boost include files")
-set(BOOST_UNIT_TEST_LIB "" CACHE STRING "Full path to Boost unit test library")
+set(BOOST_INCLUDE_DIR "" CACHE PATH "Directory containing Boost header files")
+set(BOOST_UNIT_TEST_LIB "" CACHE PATH "Full path to Boost unit test library")
 
 # BLAS
 set(BLAS_LIBRARIES "" CACHE STRING "Semicolon-separated list of full paths to BLAS libs")
+set(BLAS_INCLUDE_DIR "" CACHE PATH "Directory containing BLAS header files (used only with MKL, GotoBLAS and OpenBLAS)")
 
 # LAPACK
 set(LAPACK_LIBRARIES "" CACHE STRING "Semicolon-separated list of full paths to LAPACK libs")
+set(LAPACK_INCLUDE_DIR "" CACHE PATH "Directory containing LAPACK header files (used only with MKL, GotoBLAS and OpenBLAS)")
 
 # ARMADILLO
-set(ARMADILLO_INCLUDE_DIR "" CACHE STRING "Include directory for Armadillo")
+set(ARMADILLO_INCLUDE_DIR "" CACHE PATH "Directory containing Armadillo header files")
 
 # Threading building blocks
-set(TBB_INCLUDE_DIR "" CACHE STRING "Full path to Intel TBB include directory")
-set(TBB_LIBRARY "" CACHE STRING "Full path to the TBB library")
-set(TBB_LIBRARY_DEBUG "" CACHE STRING "Full path to the TBB debug library")
+set(TBB_INCLUDE_DIR "" CACHE PATH "Directory containing Intel TBB header files")
+set(TBB_LIBRARY "" CACHE PATH "Full path to the TBB library")
+set(TBB_LIBRARY_DEBUG "" CACHE PATH "Full path to the TBB debug library")
 
 # Ahmed (optional, used only if WITH_AHMED is set)
 if (WITH_AHMED)
-	set(AHMED_INCLUDE_DIR "" CACHE STRING "Full path to the AHMED include directory")
-	set(AHMED_LIB "" CACHE STRING "Full path to AHMED library")
+    set(AHMED_INCLUDE_DIR "" CACHE PATH "Full path to the AHMED include directory")
+    set(AHMED_LIB "" CACHE PATH "Full path to AHMED library")
 endif ()
 
 # Dune
@@ -28,7 +30,7 @@ find_library(LIB_DUNE_GRID dunegrid ${CMAKE_INSTALL_PREFIX}/bempp/lib)
 file(GLOB_RECURSE DUNE_HEADERS ${CMAKE_INSTALL_PREFIX}/bempp/include/dune/*.hh)
 
 # Trilinos
-set(TRILINOS_CMAKE_PATH "" CACHE STRING "Directory containing TrilinosConfig.cmake")
+set(TRILINOS_CMAKE_PATH "" CACHE PATH "Directory containing TrilinosConfig.cmake")
 include(${TRILINOS_CMAKE_PATH}/TrilinosConfig.cmake)
 
 
