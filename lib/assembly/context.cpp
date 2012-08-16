6bfcebec10dd89a2831cc75ae747faeab12d0692
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-08-16 12:52:13 +0100

    Documented the Context class.

--- lib/assembly/context.cpp
+++ lib/assembly/context.cpp
@@ -20,20 +20,11 @@
 
 #include "context.hpp"
 #include "../fiber/explicit_instantiation.hpp"
-//#include "numerical_quadrature_strategy.hpp"
 
 #include <boost/make_shared.hpp>
 
 namespace Bempp
 {
-
-//template <typename BasisFunctionType, typename ResultType>
-//shared_ptr<const Context<BasisFunctionType, ResultType> >
-//Context<BasisFunctionType, ResultType>::m_defaultContext(
-//        boost::make_shared<Context<BasisFunctionType, ResultType>(
-//            AssemblyOptions(),
-//            boost::make_shared<DefaultQuadratureStrategyForOperatorsOnSurfaces<
-//            BasisFunctionType, ResultType> >()));
 
 template <typename BasisFunctionType, typename ResultType>
 Context<BasisFunctionType, ResultType>::Context(
@@ -44,18 +35,8 @@ Context<BasisFunctionType, ResultType>::Context(
 {
     if (quadStrategy.get() == 0)
         throw std::invalid_argument("Context::Context(): "
-                                    "quadStrategy cannot be null");
+                                    "quadStrategy must not be null");
 }
-
-//template <typename BasisFunctionType, typename ResultType>
-//void Context<BasisFunctionType, ResultType>::setDefaultContext(
-//        const shared_ptr<const Context>& newContext)
-//{
-//    if (newContext.get() == 0)
-//        throw std::invalid_argument("Context::setDefaultContext(): "
-//                                    "new default context cannot be null");
-//    m_defaultContext = newContext;
-//}
 
 FIBER_INSTANTIATE_CLASS_TEMPLATED_ON_BASIS_AND_RESULT(Context);
 
