b7d747d9df0f4cb1679ab1983a455ede4f07b4ec
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-08-17 12:27:17 +0100

    Improved the documentation of Space.

--- lib/space/space.hpp
+++ lib/space/space.hpp
@@ -53,12 +53,23 @@ template <int codim> class EntityPointer;
 
 template <typename ValueType> class DiscreteBoundaryOperator;
 
+/** \brief Function space.
+ *
+ *  This class represents a (finite) space of functions defined on a grid.
+ *
+ *  \tparam BasisFunctionType
+ *    Type used to represent components of functions belonging to this space.
+ *    Can be one of: \c float, \c * double, <tt>std::complex<float></tt> and
+ *    <tt>std::complex<double></tt>. */
 template <typename BasisFunctionType>
 class Space
 {
 public:
+    /** \brief Type used to represent coordinates. */
     typedef typename Fiber::ScalarTraits<BasisFunctionType>::RealType CoordinateType;
+    /** \brief Equivalent to std::complex<CoordinateType>. */
     typedef typename Fiber::ScalarTraits<BasisFunctionType>::ComplexType ComplexType;
+    /** \brief Appropriate instantiation of Fiber::CollectionOfBasisTransformations. */
     typedef Fiber::CollectionOfBasisTransformations<CoordinateType>
     CollectionOfBasisTransformations;
 
@@ -66,37 +77,75 @@ public:
     // variant it is necessary to check whether the element is triangular
     // or quadrilateral. Also, requests for element refinement should probably
     // be made via Space rather than via Grid.
+    /** \brief Constructor.
+     *
+     *  \param[in] grid Grid on which functions from this space should be defined.
+     *
+     *  The supplied grid must remain valid until the Space object is destructed.
+     *
+     *  \todo The grid should be passed via a shared pointer instead of via a
+     *  reference.
+     */
     explicit Space(Grid& grid);
 
+    /** \brief Destructor. */
     virtual ~Space();
 
     /** @name Attributes
     @{ */
 
-    /** \brief Dimension of the surface on which the functions are defined. */
+    /** \brief Dimension of the grid on which functions from this space are defined. */
     virtual int domainDimension() const = 0;
+
     /** \brief Dimension of the codomain of the functions.
 
     In other words, number of components of the values of the functions.
     (E.g. H1 space -> 1, H(curl) space on a 2D surface -> 2). */
     virtual int codomainDimension() const = 0;
 
-    /** \brief Reference to the grid on which the functions are defined. */
+    /** \brief Reference to the grid on which the functions from this space are defined. */
     const Grid& grid() const { return m_grid; }
 
     /** \brief Reference to the basis attached to the specified element. */
     virtual const Fiber::Basis<BasisFunctionType>& basis(const Entity<0>& element) const = 0;
 
-    /** \brief Basis-function transformation returning values of the shape
-     *  functions of this space. */
+    /** \brief Transformation mapping basis functions to shape functions.
+     *
+     *  This function returns a CollectionOfBasisTransformations object
+     *  consisting of a single transformation that maps values of basis
+     *  functions defined on a reference element to those of *shape functions*
+     *  defined on a particular element of the grid.
+     *
+     *  This transformation is the identity for spaces of scalar-valued
+     *  functions, but may be more complicated for spaces of vector-valued
+     *  functions, e.g. H(curl).
+     *
+     *  \todo Perhaps change the name of this method to something more
+     *  understandable, like basisToShapeFunctionTransformation.
+     */
     virtual const CollectionOfBasisTransformations&
     shapeFunctionValue() const = 0;
 
     /** @}
         @name Element order management
         @{ */
+
+    /** \brief Set the variant of element \p element to \p variant.
+     *
+     *  The element variant determines the set of basis functions defined on
+     *  the element (e.g. maximum polynomial order). Different subclasses of
+     *  Space interpret the \p variant argument in different ways; for more
+     *  information, see the documentation of these subclasses.
+     *
+     *  \note Calling this function only makes sense for subclasses implementing
+     *  adaptive function spaces. Currently there are no such subclasses. */
     virtual void setElementVariant(const Entity<0>& element, ElementVariant variant) = 0;
+
+    /** \brief Return current variant of element \p element.
+     *
+     *  See the documentation of setElementVariant() for more information. */
     virtual ElementVariant elementVariant(const Entity<0>& element) const = 0;
+
     // additional functions for e.g. increasing polynomial order of all elements
     // ...
 
@@ -110,44 +159,119 @@ public:
     /** \brief True if assignDofs() has been called before, false otherwise. */
     virtual bool dofsAssigned() const = 0;
 
-    /** \brief Total number of local degrees of freedom (on all elements). */
+    /** \brief Total number of local degrees of freedom on all elements. */
     virtual size_t flatLocalDofCount() const = 0;
 
     /** \brief Number of global degrees of freedom.
-
-        \note Must not be called before assignDofs(). */
+     *
+     *  An exception is thrown if assignDofs() has not been called prior to
+     *  calling this function. */
     virtual size_t globalDofCount() const = 0;
+
+    /** \brief Map local degrees of freedom residing on an element to global degrees of freedom.
+     *
+     *  \param[in] element An element of the grid grid().
+     *  \param[out] dofs   Indices of the global degrees of freedom
+     *                     corresponding to the local degrees of freedom
+     *                     residing on \p element. */
     virtual void getGlobalDofs(const Entity<0>& element,
                             std::vector<GlobalDofIndex>& dofs) const = 0;
 
+    /** \brief Map global degrees of freedom to local degrees of freedom.
+     *
+     *  \param[in] globalDofs
+     *     Vector containing indices of global degrees of freedom.
+     *
+     *  \param[out] localDofs
+     *     Vector whose <tt>i</tt>th element is the vector containing all the
+     *     local degrees of freedom that are mapped to the global
+     *     degree of freedom <tt>globalDofs[i]</tt>.
+     *
+     *  Note that a local degree of freedom (LocalDof) is a combination of an
+     *  EntityIndex and LocalDofIndex, as explained in its documentation. */
     virtual void global2localDofs(
             const std::vector<GlobalDofIndex>& globalDofs,
             std::vector<std::vector<LocalDof> >& localDofs) const = 0;
+
+    /** \brief Map indices of flat local degrees of freedom to local degrees of freedom.
+     *
+     *  \param[in] flatLocalDofs
+     *     Vector containing flat indices of local degrees of freedom.
+     *
+     *  \param[out] localDofs
+     *     Vector whose <tt>i</tt>th element is the local degree of freedom
+     *     with flat index given by <tt>flatLocalDofs[i]</tt>. */
     virtual void flatLocal2localDofs(
             const std::vector<FlatLocalDofIndex>& flatLocalDofs,
             std::vector<LocalDof>& localDofs) const = 0;
 
-    // These functions is used only by the ACA assembler.
+    // These functions are used only by the ACA assembler.
     // For the moment, Point will always be 3D, independently from the
     // actual dimension of the space. Once Ahmed's bemcluster is made dimension-
     // independent, we may come up with a more elegant solution.
+    /** \brief Retrieve positions of global degrees of freedom.
+     *
+     *  \param[out] Vector whose <em>i</em>th element contains the coordinates
+     *  of the point taken to be the ``position'' (in some sense) of
+     *  <em>i</em>th global degree of freedom.
+     *
+     *  \note This function is intended as a helper for clustering algorithms
+     *  used in matrix compression algorithms such as adaptive cross
+     *  approximation. */
     virtual void getGlobalDofPositions(
             std::vector<Point3D<CoordinateType> >& positions) const = 0;
+
+    /** \brief Retrieve positions of local degrees of freedom ordered by their
+     *  flat index.
+     *
+     *  \param[out] Vector whose <em>i</em>th element contains the coordinates
+     *  of the point taken to be the ``position'' (in some sense) of
+     *  the local degree of freedom with flat index <em>i</em>.
+     *
+     *  \note This function is intended as a helper for clustering algorithms
+     *  used in matrix compression algorithms such as adaptive cross
+     *  approximation. */
     virtual void getFlatLocalDofPositions(
             std::vector<Point3D<CoordinateType> >& positions) const = 0;
 
     /** @}
         @name Debugging
         @} */
-    virtual void dumpClusterIds(const char* fileName,
-                                const std::vector<unsigned int>& clusterIds) const = 0;
+
+    /** \brief Write a VTK file showing the distribution of global degrees of
+     *  freedom into clusters.
+     *
+     *  \param[in] fileName
+     *    Name of the VTK file to be created (without extension).
+     *  \param[in] clusterIds
+     *    Vector whose <em>i</em>th element contains the identifier of the
+     *    cluster to which <em>i</em>th global degree has been assigned.
+     *
+     *  This function generates a VTK file containing a single data series
+     *  mapping the ``positions'' (see globalDofPositions()) of global degrees
+     *  of freedom to the identifiers of the clusters to which these degrees of
+     *  freedom have been assigned. It is intended for debugging clustering
+     *  algorithms. */
+    virtual void dumpClusterIds(
+            const char* fileName,
+            const std::vector<unsigned int>& clusterIdsOfGlobalDofs) const = 0;
     /** @} */
 
 protected:
     Grid& m_grid;
 };
 
-/** \brief Get pointers to Basis objects corresponding to all elements. */
+/** \brief Get pointers to Basis objects corresponding to all elements.
+ *
+ *  \param[in] space
+ *    A Space object.
+ *  \param[out] bases
+ *    Vector whose <em>i</em>th element is a pointer to the Basis object
+ *    representing the local degrees of freedom residing on the <em>i</em>th
+ *    element of the grid on which \p space is defined.
+ *
+ *  An exception is raised if <tt>space.assignDofs()</tt> has not been called
+ *  prior to calling this function. */
 template <typename BasisFunctionType>
 void getAllBases(const Space<BasisFunctionType>& space,
         std::vector<const Fiber::Basis<BasisFunctionType>*>& bases);
