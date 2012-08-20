f0390df86b4163e6a61bbec3ecca4dbd35e9506c
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-08-20 12:49:15 +0100

    Changed types of some CMake variables.

--- .build.sh
+++ .build.sh
@@ -9,22 +9,22 @@ cd $Bempp_build_dir
 CXXFLAGS="$Main_cxxflags" CFLAGS="$Main_cflags" cmake \
     -D CMAKE_CXX_COMPILER:STRING=$Main_cxx \
     -D CMAKE_C_COMPILER:STRING=$Main_cc \
-    -D CMAKE_INSTALL_PREFIX:STRING=$Main_prefix \
+    -D CMAKE_INSTALL_PREFIX:PATH=$Main_prefix \
     -D CMAKE_BUILD_TYPE:STRING=$Bempp_build_type \
     -D WITH_AHMED:STRING=$AHMED_with_ahmed \
-    -D BOOST_INCLUDE_DIR:STRING=$Boost_include_dir \
-    -D BOOST_UNIT_TEST_LIB:STRING=$Boost_unit_test_lib \
+    -D BOOST_INCLUDE_DIR:PATH=$Boost_include_dir \
+    -D BOOST_UNIT_TEST_LIB:PATH=$Boost_unit_test_lib \
     -D BLAS_LIBRARIES:STRING=$BLAS_lib \
     -D LAPACK_LIBRARIES:STRING=$LAPACK_lib \
-    -D ARMADILLO_INCLUDE_DIR:STRING=$Armadillo_include_dir \
-    -D TBB_INCLUDE_DIR:STRING=$Tbb_include_dir \
-    -D TBB_LIBRARY:STRING=$Tbb_lib \
-    -D TBB_LIBRARY_DEBUG:STRING=$Tbb_lib_debug \
-    -D AHMED_INCLUDE_DIR:STRING=$AHMED_include_dir \
-    -D AHMED_LIB:STRING=$AHMED_lib \
-    -D PYTHON_EXECUTABLE:STRING=$Python_exe \
-    -D PYTHON_INCLUDE_DIR:STRING=$Python_include_dir \
-    -D PYTHON_NUMPY_INCLUDE_DIR:STRING=$Python_numpy_include_dir \
-    -D PYTHON_LIBRARY:STRING=$Python_lib \
-    -D TRILINOS_CMAKE_PATH:STRING=$Trilinos_cmake_path \
+    -D ARMADILLO_INCLUDE_DIR:PATH=$Armadillo_include_dir \
+    -D TBB_INCLUDE_DIR:PATH=$Tbb_include_dir \
+    -D TBB_LIBRARY:PATH=$Tbb_lib \
+    -D TBB_LIBRARY_DEBUG:PATH=$Tbb_lib_debug \
+    -D AHMED_INCLUDE_DIR:PATH=$AHMED_include_dir \
+    -D AHMED_LIB:PATH=$AHMED_lib \
+    -D PYTHON_EXECUTABLE:PATH=$Python_exe \
+    -D PYTHON_INCLUDE_DIR:PATH=$Python_include_dir \
+    -D PYTHON_NUMPY_INCLUDE_DIR:PATH=$Python_numpy_include_dir \
+    -D PYTHON_LIBRARY:PATH=$Python_lib \
+    -D TRILINOS_CMAKE_PATH:PATH=$Trilinos_cmake_path \
 $Main_root_dir
