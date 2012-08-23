bempp/bempp-legacy

commit b81abbcf5fef28fa0de30c1acfd2b15f70c15233

Added a validity check to the constructor of AbstractBoundaryOperator.

lib/assembly/abstract_boundary_operator.cpp
@@ -51,6 +51,10 @@ AbstractBoundaryOperator(const shared_ptr<const Space<BasisFunctionType> >& doma
         throw std::invalid_argument(
                 "AbstractBoundaryOperator::AbstractBoundaryOperator(): "
                 "dualToRange must not be null");
+    if (&m_range->grid() != m_dualToRange->grid())
+        throw std::invalid_argument(
+                "AbstractBoundaryOperator::AbstractBoundaryOperator(): "
+                "range and dualToRange must be defined on the same grid");
 }
 
 template <typename BasisFunctionType, typename ResultType>

commit e5ddceb9848d0e7ac6a15291dd8b70484d92f85c

Corrected a compilation error introduced in commit b81abbcf5fef28fa0de30c1acfd2b15f70c15233.

lib/assembly/abstract_boundary_operator.cpp
@@ -51,7 +51,7 @@ AbstractBoundaryOperator(const shared_ptr<const Space<BasisFunctionType> >& doma
         throw std::invalid_argument(
                 "AbstractBoundaryOperator::AbstractBoundaryOperator(): "
                 "dualToRange must not be null");
-    if (&m_range->grid() != m_dualToRange->grid())
+    if (&m_range->grid() != &m_dualToRange->grid())
         throw std::invalid_argument(
                 "AbstractBoundaryOperator::AbstractBoundaryOperator(): "
                 "range and dualToRange must be defined on the same grid");