8b5d05ea7ebb7cae1d3b91d3a2b0d81cd0f43490
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-10-31 17:39:45 +0000

    Added the GeneralElementarySingularIntegralOperator class.

---
+++ lib/assembly/general_elementary_singular_integral_operator.hpp
@@ -0,0 +1,94 @@
+// Copyright (C) 2011-2012 by the BEM++ Authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef bempp_general_elementary_singular_integral_operator_hpp
+#define bempp_general_elementary_singular_integral_operator_hpp
+
+#include "elementary_singular_integral_operator.hpp"
+
+namespace Bempp
+{
+
+template <typename BasisFunctionType_, typename KernelType_, typename ResultType_>
+class GeneralElementarySingularIntegralOperator :
+        public ElementarySingularIntegralOperator<
+            BasisFunctionType_, KernelType_, ResultType_>
+{
+    typedef ElementarySingularIntegralOperator<
+    BasisFunctionType_, KernelType_, ResultType_> Base;
+public:
+    /** \brief Type of the values of the basis functions into which functions
+     *  acted upon by the operator are expanded. */
+    typedef typename Base::BasisFunctionType BasisFunctionType;
+    /** \brief Type of the values of kernel functions. */
+    typedef typename Base::KernelType KernelType;
+    /** \copydoc ElementaryIntegralOperator::ResultType */
+    typedef typename Base::ResultType ResultType;
+    /** \copydoc ElementaryIntegralOperator::CoordinateType */
+    typedef typename Base::CoordinateType CoordinateType;
+    /** \copydoc ElementaryIntegralOperator::CollectionOfBasisTransformations */
+    typedef typename Base::CollectionOfBasisTransformations
+    CollectionOfBasisTransformations;
+    /** \copydoc ElementaryIntegralOperator::CollectionOfKernels */
+    typedef typename Base::CollectionOfKernels CollectionOfKernels;
+    /** \copydoc ElementaryIntegralOperator::TestKernelTrialIntegral */
+    typedef typename Base::TestKernelTrialIntegral TestKernelTrialIntegral;
+
+    template <typename KernelFunctor,
+              typename TestTransformationsFunctor,
+              typename TrialTransformationsFunctor,
+              typename IntegrandFunctor>
+    GeneralElementarySingularIntegralOperator(
+            const shared_ptr<const Space<BasisFunctionType_> >& domain,
+            const shared_ptr<const Space<BasisFunctionType_> >& range,
+            const shared_ptr<const Space<BasisFunctionType_> >& dualToRange,
+            const std::string& label,
+            int symmetry,
+            const KernelFunctor& kernelFunctor,
+            const TestTransformationsFunctor& testTransformationsFunctor,
+            const TrialTransformationsFunctor& trialTransformationsFunctor,
+            const IntegrandFunctor& integrandFunctor,
+            const shared_ptr<const AbstractBoundaryOperatorId>& id =
+            shared_ptr<const AbstractBoundaryOperatorId>());
+
+    virtual const CollectionOfKernels& kernels() const
+    { return *m_kernels; }
+    virtual const CollectionOfBasisTransformations& testTransformations() const
+    { return *m_testTransformations; }
+    virtual const CollectionOfBasisTransformations& trialTransformations() const
+    { return *m_trialTransformations; }
+    virtual const TestKernelTrialIntegral& integral() const
+    { return *m_integral; }
+    shared_ptr<const AbstractBoundaryOperatorId> id() const
+    { return m_id; }
+
+private:
+    /** \cond PRIVATE */
+    shared_ptr<CollectionOfKernels> m_kernels;
+    shared_ptr<CollectionOfBasisTransformations> m_testTransformations;
+    shared_ptr<CollectionOfBasisTransformations> m_trialTransformations;
+    shared_ptr<TestKernelTrialIntegral> m_integral;
+    shared_ptr<const AbstractBoundaryOperatorId> m_id;
+    /** \endcond */
+};
+
+} // namespace Bempp
+
+#endif
---
+++ lib/assembly/general_elementary_singular_integral_operator_imp.hpp
@@ -0,0 +1,113 @@
+// Copyright (C) 2011-2012 by the BEM++ Authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef bempp_general_elementary_singular_integral_operator_imp_hpp
+#define bempp_general_elementary_singular_integral_operator_imp_hpp
+
+#include "general_elementary_singular_integral_operator.hpp"
+
+#include "../fiber/default_collection_of_kernels.hpp"
+#include "../fiber/default_collection_of_basis_transformations.hpp"
+#include "../fiber/default_test_kernel_trial_integral.hpp"
+
+namespace Bempp
+{
+
+template <typename BasisFunctionType_, typename KernelType_, typename ResultType_>
+template <typename KernelFunctor,
+          typename TestTransformationsFunctor,
+          typename TrialTransformationsFunctor,
+          typename IntegrandFunctor>
+GeneralElementarySingularIntegralOperator<
+BasisFunctionType_, KernelType_, ResultType_>::
+GeneralElementarySingularIntegralOperator(
+        const shared_ptr<const Space<BasisFunctionType_> >& domain,
+        const shared_ptr<const Space<BasisFunctionType_> >& range,
+        const shared_ptr<const Space<BasisFunctionType_> >& dualToRange,
+        const std::string& label,
+        int symmetry,
+        const KernelFunctor& kernelFunctor,
+        const TestTransformationsFunctor& testTransformationsFunctor,
+        const TrialTransformationsFunctor& trialTransformationsFunctor,
+        const IntegrandFunctor& integrandFunctor,
+        const shared_ptr<const AbstractBoundaryOperatorId>& id) :
+    Base(domain, range, dualToRange, label, symmetry),
+    m_kernels(
+        new Fiber::DefaultCollectionOfKernels<KernelFunctor>(kernelFunctor)),
+    m_testTransformations(
+        new Fiber::DefaultCollectionOfBasisTransformations<TestTransformationsFunctor>(
+            testTransformationsFunctor)),
+    m_trialTransformations(
+        new Fiber::DefaultCollectionOfBasisTransformations<TrialTransformationsFunctor>(
+            trialTransformationsFunctor)),
+    m_integral(
+        new Fiber::DefaultTestKernelTrialIntegral<IntegrandFunctor>(
+            integrandFunctor)),
+    m_id(id)
+{
+}
+
+//template <typename BasisFunctionType_, typename KernelType_, typename ResultType_>
+//const typename GeneralElementarySingularIntegralOperator<
+//    BasisFunctionType_, KernelType_, ResultType_>::CollectionOfKernels&
+//GeneralElementarySingularIntegralOperator<
+//BasisFunctionType_, KernelType_, ResultType_>::kernels() const
+//{
+//    return *m_kernels;
+//}
+
+//template <typename BasisFunctionType_, typename KernelType_, typename ResultType_>
+//const typename GeneralElementarySingularIntegralOperator<
+//    BasisFunctionType_, KernelType_, ResultType_>::CollectionOfBasisTransformations&
+//GeneralElementarySingularIntegralOperator<
+//BasisFunctionType_, KernelType_, ResultType_>::testTransformations() const
+//{
+//    return *m_testTransformations;
+//}
+
+//template <typename BasisFunctionType_, typename KernelType_, typename ResultType_>
+//const typename GeneralElementarySingularIntegralOperator<
+//    BasisFunctionType_, KernelType_, ResultType_>::CollectionOfBasisTransformations&
+//GeneralElementarySingularIntegralOperator<
+//BasisFunctionType_, KernelType_, ResultType_>::trialTransformations() const
+//{
+//    return *m_trialTransformations;
+//}
+
+//template <typename BasisFunctionType_, typename KernelType_, typename ResultType_>
+//const typename GeneralElementarySingularIntegralOperator<
+//    BasisFunctionType_, KernelType_, ResultType_>::TestKernelTrialIntegral&
+//GeneralElementarySingularIntegralOperator<
+//BasisFunctionType_, KernelType_, ResultType_>::integral() const
+//{
+//    return *m_integral;
+//}
+
+//template <typename BasisFunctionType_, typename KernelType_, typename ResultType_>
+//shared_ptr<const AbstractBoundaryOperatorId>
+//GeneralElementarySingularIntegralOperator<
+//BasisFunctionType_, KernelType_, ResultType_>::id() const
+//{
+//    return m_id;
+//}
+
+} // namespace Bempp
+
+#endif
