c5602adfaf63c5c56c216008875f235a78cc7795
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2012-12-10 17:56:07 +0000

    Added two more tests of ACA with globalAssemblyBeforeCompression = false.

--- tests/unit/assembly/test_aca_mode_assembly.cpp
+++ tests/unit/assembly/test_aca_mode_assembly.cpp
@@ -30,6 +30,8 @@
 #include "assembly/laplace_3d_adjoint_double_layer_boundary_operator.hpp"
 #include "assembly/laplace_3d_double_layer_boundary_operator.hpp"
 #include "assembly/laplace_3d_hypersingular_boundary_operator.hpp"
+#include "assembly/laplace_3d_single_layer_boundary_operator.hpp"
+#include "assembly/helmholtz_3d_hypersingular_boundary_operator.hpp"
 #include "assembly/numerical_quadrature_strategy.hpp"
 #include "grid/grid_factory.hpp"
 #include "space/piecewise_constant_scalar_space.hpp"
@@ -88,6 +90,55 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(aca_of_assembled_operator_agrees_with_dense_assemb
     BoundaryOperator<BFT, RT> opAca =
             laplace3dDoubleLayerBoundaryOperator<BFT, RT>(
                 contextAca, pwiseLinears, pwiseLinears, pwiseConstants);
+    arma::Mat<RT> weakFormAca = opAca.weakForm()->asMatrix();
+
+    BOOST_CHECK(check_arrays_are_close<ValueType>(
+                    weakFormDense, weakFormAca, 2. * acaOptions.eps));
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(aca_of_disassembled_single_layer_potential_operator_agrees_with_dense_assembly_for_614_element_mesh,
+                              ValueType, result_types)
+{
+    typedef ValueType RT;
+    typedef typename ScalarTraits<ValueType>::RealType RealType;
+    typedef RealType BFT;
+
+    GridParameters params;
+    params.topology = GridParameters::TRIANGULAR;
+    shared_ptr<Grid> grid = GridFactory::importGmshGrid(
+        params, "../../examples/meshes/sphere-h-0.2.msh", false /* verbose */);
+
+    shared_ptr<Space<BFT> > pwiseConstants(
+        new PiecewiseConstantScalarSpace<BFT>(grid));
+    shared_ptr<Space<BFT> > pwiseLinears(
+        new PiecewiseLinearContinuousScalarSpace<BFT>(grid));
+
+    AccuracyOptions accuracyOptions;
+    accuracyOptions.doubleRegular.setRelativeQuadratureOrder(2);
+    shared_ptr<NumericalQuadratureStrategy<BFT, RT> > quadStrategy(
+                new NumericalQuadratureStrategy<BFT, RT>(accuracyOptions));
+
+    AssemblyOptions assemblyOptionsDense;
+    assemblyOptionsDense.setVerbosityLevel(VerbosityLevel::LOW);
+    shared_ptr<Context<BFT, RT> > contextDense(
+        new Context<BFT, RT>(quadStrategy, assemblyOptionsDense));
+
+    BoundaryOperator<BFT, RT> opDense =
+            laplace3dSingleLayerBoundaryOperator<BFT, RT>(
+                contextDense, pwiseLinears, pwiseConstants, pwiseLinears);
+    arma::Mat<RT> weakFormDense = opDense.weakForm()->asMatrix();
+
+    AssemblyOptions assemblyOptionsAca;
+    assemblyOptionsAca.setVerbosityLevel(VerbosityLevel::LOW);
+    AcaOptions acaOptions;
+    acaOptions.globalAssemblyBeforeCompression = false;
+    assemblyOptionsAca.switchToAcaMode(acaOptions);
+    shared_ptr<Context<BFT, RT> > contextAca(
+        new Context<BFT, RT>(quadStrategy, assemblyOptionsAca));
+
+    BoundaryOperator<BFT, RT> opAca =
+            laplace3dSingleLayerBoundaryOperator<BFT, RT>(
+                contextAca, pwiseLinears, pwiseConstants, pwiseLinears);
     arma::Mat<RT> weakFormAca = opAca.weakForm()->asMatrix();
 
     BOOST_CHECK(check_arrays_are_close<ValueType>(
@@ -194,6 +245,8 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(aca_of_disassembled_adjoint_double_potential_opera
 
 BOOST_AUTO_TEST_CASE_TEMPLATE(aca_of_disassembled_hypersingular_operator_agrees_with_dense_assembly_for_614_element_mesh,
                               ValueType, result_types)
+BOOST_AUTO_TEST_CASE_TEMPLATE(aca_of_disassembled_helmholtz_hypersingular_operator_agrees_with_dense_assembly_for_614_element_mesh,
+                              ValueType, complex_result_types)
 {
     typedef ValueType RT;
     typedef typename ScalarTraits<ValueType>::RealType RealType;
@@ -219,9 +272,11 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(aca_of_disassembled_hypersingular_operator_agrees_
     shared_ptr<Context<BFT, RT> > contextDense(
         new Context<BFT, RT>(quadStrategy, assemblyOptionsDense));
 
+    const RT waveNumber = 3.;
     BoundaryOperator<BFT, RT> opDense =
-            laplace3dHypersingularBoundaryOperator<BFT, RT>(
-                contextDense, pwiseLinears, pwiseConstants, pwiseLinears);
+            helmholtz3dHypersingularBoundaryOperator<BFT>(
+                contextDense, pwiseLinears, pwiseConstants, pwiseLinears,
+                waveNumber);
     arma::Mat<RT> weakFormDense = opDense.weakForm()->asMatrix();
 
     AssemblyOptions assemblyOptionsAca;
@@ -233,8 +288,9 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(aca_of_disassembled_hypersingular_operator_agrees_
         new Context<BFT, RT>(quadStrategy, assemblyOptionsAca));
 
     BoundaryOperator<BFT, RT> opAca =
-            laplace3dHypersingularBoundaryOperator<BFT, RT>(
-                contextAca, pwiseLinears, pwiseConstants, pwiseLinears);
+            helmholtz3dHypersingularBoundaryOperator<BFT>(
+                contextAca, pwiseLinears, pwiseConstants, pwiseLinears,
+                waveNumber);
     arma::Mat<RT> weakFormAca = opAca.weakForm()->asMatrix();
 
     BOOST_CHECK(check_arrays_are_close<ValueType>(
