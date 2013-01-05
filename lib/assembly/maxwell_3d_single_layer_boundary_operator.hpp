9345f293022ac56f7e4c782ae014f898330caa15
wsmigaj <w.smigaj@ucl.ac.uk>
2013-01-05 18:33:42 +0100

    Implemented the single-layer boundary op. for Maxwell equations.

---
+++ lib/assembly/maxwell_3d_single_layer_boundary_operator.hpp
@@ -0,0 +1,87 @@
+// Copyright (C) 2011-2012 by the BEM++ Authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef bempp_maxwell_3d_single_layer_boundary_operator_hpp
+#define bempp_maxwell_3d_single_layer_boundary_operator_hpp
+
+#include "boundary_operator.hpp"
+#include "helmholtz_3d_operators_common.hpp"
+#include "symmetry.hpp"
+
+#include "../common/scalar_traits.hpp"
+
+namespace Bempp
+{
+
+/** \brief Construct a single-layer boundary operator for Maxwell equations in 3D.
+ *
+ *  \param[in] context
+ *    A Context object that will be used to build the weak form of the
+ *    boundary operator when necessary.
+ *  \param[in] domain
+ *    Function space being the domain of the boundary operator.
+ *  \param[in] range
+ *    Function space being the range of the boundary operator.
+ *  \param[in] dualToRange
+ *    Function space dual to the the range of the boundary operator.
+ *  \param[in] waveNumber
+ *    Wave number. See \ref maxwell_3d for its definition.
+ *  \param[in] label
+ *    Textual label of the operator. If empty, a unique label is generated
+ *    automatically.
+ *  \param[in] symmetry
+ *    Symmetry of the weak form of the operator. Can be any combination of the
+ *    flags defined in the enumeration type Symmetry.
+ *  \param[in] useInterpolation
+ *    If set to \p false (default), the standard exp() function will be used to
+ *    evaluate the exponential factor occurring in the kernel. If set to \p
+ *    true, the exponential factor will be evaluated by piecewise-cubic
+ *    interpolation of values calculated in advance on a regular grid. This
+ *    normally speeds up calculations, but might result in a loss of accuracy.
+ *    This is an experimental feature: use it at your own risk.
+ *  \param[in] interpPtsPerWavelength
+ *    If \p useInterpolation is set to \p true, this parameter determines the
+ *    number of points per "effective wavelength" (defined as \f$2\pi/|k|\f$,
+ *    where \f$k\f$ = \p waveNumber) used to construct the interpolation grid.
+ *    The default value (5000) is normally enough to reduce the relative or
+ *    absolute error, *whichever is smaller*, below 100 * machine precision. If
+ *    \p useInterpolation is set to \p false, this parameter is ignored.
+ *
+ *  None of the shared pointers may be null and the spaces \p range and \p
+ *  dualToRange must be defined on the same grid, otherwise an exception is
+ *  thrown. */
+template <typename BasisFunctionType>
+BoundaryOperator<BasisFunctionType,
+    typename ScalarTraits<BasisFunctionType>::ComplexType>
+maxwell3dSingleLayerBoundaryOperator(
+        const shared_ptr<const Context<BasisFunctionType,
+        typename ScalarTraits<BasisFunctionType>::ComplexType> >& context,
+        const shared_ptr<const Space<BasisFunctionType> >& domain,
+        const shared_ptr<const Space<BasisFunctionType> >& range,
+        const shared_ptr<const Space<BasisFunctionType> >& dualToRange,
+        typename ScalarTraits<BasisFunctionType>::ComplexType waveNumber,
+        const std::string& label = "",
+        int symmetry = NO_SYMMETRY,
+        bool useInterpolation = false,
+        int interpPtsPerWavelength = DEFAULT_HELMHOLTZ_INTERPOLATION_DENSITY);
+
+} // namespace Bempp
+
+#endif
---
+++ lib/fiber/modified_maxwell_3d_single_layer_potential_kernel_interpolated_functor.hpp
@@ -0,0 +1,93 @@
+// Copyright (C) 2011-2012 by the BEM++ Authors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef fiber_modified_maxwell_3d_single_layer_potential_kernel_interpolated_functor_hpp
+#define fiber_modified_maxwell_3d_single_layer_potential_kernel_interpolated_functor_hpp
+
+#include "../common/common.hpp"
+
+#include "geometrical_data.hpp"
+#include "scalar_traits.hpp"
+
+#include "modified_helmholtz_3d_single_layer_potential_kernel_interpolated_functor.hpp"
+
+namespace Fiber
+{
+
+/** \ingroup modified_maxwell_3d
+ *  \ingroup functors
+ *  \brief Kernel collection functor for the SLP of the modified Maxwell
+ *  equations in 3D.
+ *
+ *  The functor evaluates two kernels: the single-layer potential kernel of the
+ *  modified Helmholtz equation and the single-layer potential kernel divided by
+ *  m_waveNumber**2. It uses interpolation to speed up calculations.
+ *
+ *  \tparam ValueType Type used to represent the values of the kernel. It can
+ *  be one of: \c float, \c double, <tt>std::complex<float></tt> and
+ *  <tt>std::complex<double></tt>. Note that setting \p ValueType to a real
+ *  type implies that the wave number will also be purely real.
+ *
+ *  \see modified_maxwell_3d
+ */
+template <typename ValueType_>
+class ModifiedMaxwell3dSingleLayerPotentialKernelInterpolatedFunctor
+{
+public:
+    typedef ValueType_ ValueType;
+    typedef typename ScalarTraits<ValueType>::RealType CoordinateType;
+
+    ModifiedMaxwell3dSingleLayerPotentialKernelInterpolatedFunctor(
+            ValueType waveNumber,
+            CoordinateType maxDist, int interpPtsPerWavelength) :
+        m_slpKernel(waveNumber, maxDist, interpPtsPerWavelength)
+    {}
+
+    int kernelCount() const { return 2; }
+    int kernelRowCount(int /* kernelIndex */) const { return 1; }
+    int kernelColCount(int /* kernelIndex */) const { return 1; }
+
+    void addGeometricalDependencies(size_t& testGeomDeps, size_t& trialGeomDeps) const {
+        m_slpKernel.addGeometricalDependencies(testGeomDeps, trialGeomDeps);
+    }
+
+    ValueType waveNumber() const { return m_slpKernel.waveNumber(); }
+
+    template <template <typename T> class CollectionOf2dSlicesOfNdArrays>
+    void evaluate(
+            const ConstGeometricalDataSlice<CoordinateType>& testGeomData,
+            const ConstGeometricalDataSlice<CoordinateType>& trialGeomData,
+            CollectionOf2dSlicesOfNdArrays<ValueType>& result) const {
+        // This will put the value of the SLP kernel in result[0](0, 0)
+        m_slpKernel.evaluate(testGeomData, trialGeomData, result);
+        result[1](0, 0) = result[0](0, 0) /
+            (m_slpKernel.waveNumber() * m_slpKernel.waveNumber());
+    }
+
+private:
+    /** \cond PRIVATE */
+    ModifiedHelmholtz3dSingleLayerPotentialKernelInterpolatedFunctor<ValueType>
+        m_slpKernel;
+    /** \endcond */
+};
+
+} // namespace Fiber
+
+#endif
