24493af2368c4732b5dac81cdda68782e5b2f160
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2013-05-07 13:16:28 +0100

    Added IndexPermutation::permutationMatrix().

--- lib/assembly/index_permutation.hpp
+++ lib/assembly/index_permutation.hpp
@@ -25,6 +25,9 @@
 #include "../common/common.hpp"
 
 #include "../common/armadillo_fwd.hpp"
+#include "../common/shared_ptr.hpp"
+
+class Epetra_CrsMatrix;
 
 namespace Bempp
 {
@@ -95,6 +98,8 @@ public:
     size_t size() const {
         return m_permutedIndices.size();
     }
+
+    shared_ptr<const Epetra_CrsMatrix> permutationMatrix() const;
 
 private:
     const std::vector<unsigned int> m_permutedIndices;
