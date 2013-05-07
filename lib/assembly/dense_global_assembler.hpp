ed541caab8cd2e8c85d80ee87422a488a9abb964
wojciech.smigaj <w.smigaj@ucl.ac.uk>
2013-05-07 11:21:50 +0100

    Extracted dense assembly to a separate helper class.

---
+++ lib/assembly/dense_global_assembler.hpp
@@ -0,0 +1,47 @@
+#ifndef bempp_dense_global_assembler_hpp
+#define bempp_dense_global_assembler_hpp
+
+#include "../common/common.hpp"
+
+#include <memory>
+
+namespace Fiber
+{
+
+/** \cond FORWARD_DECL */
+template <typename ResultType> class LocalAssemblerForOperators;
+/** \endcond */
+
+} // namespace Fiber
+
+namespace Bempp
+{
+
+/** \cond FORWARD_DECL */
+class AssemblyOptions;
+template <typename ValueType> class DiscreteBoundaryOperator;
+template <typename BasisFunctionType> class Space;
+template <typename BasisFunctionType, typename ResultType> class Context;
+/** \endcond */
+
+/** \ingroup weak_form_assembly_internal
+ *  \brief Dense-mode assembler.
+ */
+template <typename BasisFunctionType, typename ResultType>
+class DenseGlobalAssembler
+{
+public:
+    typedef Fiber::LocalAssemblerForOperators<ResultType>
+    LocalAssemblerForBoundaryOperators;
+
+    static std::auto_ptr<DiscreteBoundaryOperator<ResultType> >
+    assembleDetachedWeakForm(
+            const Space<BasisFunctionType>& testSpace,
+            const Space<BasisFunctionType>& trialSpace,
+            LocalAssemblerForBoundaryOperators& assembler,
+            const Context<BasisFunctionType, ResultType>& context);
+};
+
+} // namespace Bempp
+
+#endif
