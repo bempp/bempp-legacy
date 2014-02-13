3423b071e88866e03c68d5487ee235c0e31ac6cf
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-02-13 16:15:21 +0000

    Unsets TBB variable if partially found
    Makes sure that TBB is found fully or not at all. Otherwise could end up with debug library from one
    version and release from another.

--- cmake/FindTBB.cmake
+++ cmake/FindTBB.cmake
@@ -6,10 +6,10 @@
 # - TBB_MALLOC_LIBRARY_DEBUG
 # - TBB_INCLUDE_DIR
 
-find_library(TBB_LIBRARY NAMES libtbb.so libtbb.dylib)
-find_library(TBB_LIBRARY_DEBUG NAMES libtbb_debug.so libtbb_debug.dylib)
-find_library(TBB_MALLOC_LIBRARY NAMES libtbbmalloc.so libtbbmalloc.dylib)
-find_library(TBB_MALLOC_LIBRARY_DEBUG NAMES libtbbmalloc_debug.so libtbbmalloc_debug.dylib)
+find_library(TBB_LIBRARY NAMES tbb)
+find_library(TBB_LIBRARY_DEBUG NAMES tbb_debug)
+find_library(TBB_MALLOC_LIBRARY NAMES tbbmalloc)
+find_library(TBB_MALLOC_LIBRARY_DEBUG NAMES tbbmalloc_debug)
 find_path(TBB_INCLUDE_DIR tbb.h PATH_SUFFIXES tbb)
 
 include(FindPackageHandleStandardArgs)
@@ -19,3 +19,12 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(
   TBB_LIBRARY TBB_LIBRARY_DEBUG TBB_MALLOC_LIBRARY TBB_MALLOC_LIBRARY_DEBUG
   TBB_INCLUDE_DIR
 )
+
+# Unsets values if not found, so we don't end up with mixes of library from one place and another.
+if(NOT TBB_FOUND)
+  unset(TBB_LIBRARY CACHE)
+  unset(TBB_LIBRARY_DEBUG CACHE)
+  unset(TBB_MALLOC_LIBRARY CACHE)
+  unset(TBB_MALLOC_LIBRARY_DEBUG CACHE)
+  unset(TBB_INCLUDE_DIR CACHE)
+endif()
