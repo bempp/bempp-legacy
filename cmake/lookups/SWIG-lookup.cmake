8aa05d544a47c9d2009792f2da41f3f53e1d8f22
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-04-10 14:52:14 +0100

    Missing hunk to download pcre

--- cmake/lookups/SWIG-lookup.cmake
+++ cmake/lookups/SWIG-lookup.cmake
@@ -1,3 +1,16 @@
+# Add a warning if pcre library is not found
+find_program(PCRE_CONFIG_EXECUTABLE pcre-config)
+if(NOT PCRE_CONFIG_EXECUTABLE)
+    if(NOT EXISTS ${EXTERNAL_ROOT}/src/SWIG/pcre-8.33.tar.gz)
+        file(WRITE "${EXTERNAL_ROOT}/src/pcre.cmake"
+            "file(DOWNLOAD\n"
+            "\"ftp://ftp.csx.cam.ac.uk/pub/software/"
+                "programming/pcre/pcre-8.33.tar.gz\"\n"
+            "\"${EXTERNAL_ROOT}/src/SWIG/pcre-8.33.tar.gz\"\n"
+            ")\n"
+        )
+    endif()
+endif()
 # Downloads SWIG
 ExternalProject_Add(
     SWIG
