e26acbc60a3504f950de88b252f9b13156f0e186
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-05-08 13:29:45 +0100

    Correct for obsolete c++11 feature
    Older gcc implemented the monotic clock, which was subsequently removed from the (then tentitative)
    standard for c++11.
    Add some code to detect its usage and typedef it to steady_clock. The latter is in the accepted
    standard.

---
+++ cmake/cpp11_project.cmake
@@ -0,0 +1,10 @@
+# Flags to compile the project with C++11
+include(AddCPP11Flags)
+
+# Makes it possible to check for C++11 features
+include(CheckCXX11Features)
+
+cxx11_feature_check("steady_clock" "obsolete/monotonic_clock")
+if(NOT HAS_CXX11_STEADY_CLOCK AND HAS_CXX11_OBSOLETE_MONOTONIC_CLOCK)
+  set(BEMPP_ADD_STEADY_CLOCK_FROM_MONOTONIC_CLOCK TRUE)
+endif()
