6320ca8c00b053283e29f3d3751a2fd6c00a15ea
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-05-08 13:09:51 +0100

    Patch boost for intel 13 c++11 :bug:
    This is a know boost and/or intel :bug:, with a [:ticket:](https://svn.boost.org/trac/boost/ticket/9417).

--- cmake/lookups/Boost-lookup.cmake
+++ cmake/lookups/Boost-lookup.cmake
@@ -1,12 +1,35 @@
+set(toolset "")
+if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+  set(toolset "toolset=intel-linux")
+endif()
+
+# Patches class for intel 13 c++11 (miss?)behavior
+# Resolution from https://svn.boost.org/trac/boost/ticket/9417
+file(WRITE "${EXTERNAL_ROOT}/src/boost.patch"
+    "--- boost/test/utils/trivial_singleton.hpp	2008-10-13 09:20:26.000000000 +0100\n"
+    "+++ boost/test/utils/trivial_singleton.new.hpp	2014-05-08 12:46:52.711942285 +0100\n"
+    "@@ -37,7 +37,7 @@\n"
+    " public:\n"
+    "     static Derived& instance() { static Derived the_inst; return the_inst; }    \n"
+    " protected:\n"
+    "-    singleton()  {}\n"
+    "+    singleton() : noncopyable() {}\n"
+    "     ~singleton() {}\n"
+    " };\n"
+)
+
+find_program(PATCH_EXECUTABLE patch REQUIRED)
 ExternalProject_Add(
     Boost
     PREFIX ${EXTERNAL_ROOT}
     # Downloads boost from url -- much faster than svn
     URL http://sourceforge.net/projects/boost/files/boost/1.55.0/boost_1_55_0.tar.bz2/download
     BUILD_IN_SOURCE 1
-    CONFIGURE_COMMAND ./bootstrap.sh
-    BUILD_COMMAND  ./b2 link=static variant=release --with-test
-    INSTALL_COMMAND ./b2 link=static variant=release --with-test --prefix=${EXTERNAL_ROOT} install
+    CONFIGURE_COMMAND ./bootstrap.sh 
+    PATCH_COMMAND ${PATCH_EXECUTABLE} -p0 < "${EXTERNAL_ROOT}/src/boost.patch"
+    BUILD_COMMAND  ./b2 ${toolset} link=static variant=release --with-test
+    INSTALL_COMMAND ./b2 ${toolset} link=static variant=release --with-test
+        --prefix=${EXTERNAL_ROOT} install
     LOG_DOWNLOAD ON
     LOG_CONFIGURE ON
     LOG_BUILD ON
