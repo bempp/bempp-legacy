7c551f99670a233cbdb9ac5ec0f2cf4dfb86605a
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-05-30 10:28:45 +0100

    Move all blas and lapack fidling to seperate file for clarity

--- cmake/BemppFindDependencies.cmake
+++ cmake/BemppFindDependencies.cmake
@@ -3,32 +3,8 @@ include(PackageLookup)
 
 # First, find general packages
 find_package(Doxygen)
-# Look for mkl libraries linked to numpy first and use those if available
-find_package(NumpyMKL)
-if(NumpyMKL_FOUND)
-    if(NumpyMKL_PREFIXES)
-        list(INSERT CMAKE_PREFIX_PATH 0 ${NumpyMKL_PREFIXES})
-        add_to_rpath(${NumpyMKL_LIBRARIES})
-    endif()
-    set(BLA_VENDOR ${NumpyMKL_VENDOR})
-    list(INSERT CMAKE_LIBRARY_PATH 0 ${NumpyMKL_LIBRARY_DIRS})
-    set(BLAS_LIBRARIES ${NumpyMKL_LIBRARIES})
-    set(LAPACK_LIBRARIES ${NumpyMKL_LIBRARIES})
-    set(LAPACK_FOUND TRUE)
-endif()
-# Then look for other possible blas libraries. The last few lines should ensure
-# we are using the numpy MKLs, if found.
-set(BLAS_INCLUDE_DIR_OPTIONAL TRUE)
-find_package(CBLAS REQUIRED)
-# Check if threading exists in library
-include(BlasThreads)
-if(NOT NumpyMKL_FOUND)
-    # Assume mkls have Lapack. Not sure this is true... but can't use numpy's mkl otherwise.
-    find_package(LAPACK REQUIRED)
-endif()
-if(NOT LAPACK_INCLUDE_DIR)
-    find_path(LAPACK_INCLUDE_DIR clapack.h)
-endif()
+# Look for lapack and blas
+include(lapack_and_blas)
 # Look for python libraries corresponding to the python interpreter
 # This step is likely not compatible with (automatic) cross-compilation
 find_package(CoherentPython REQUIRED)
---
+++ cmake/lapack_and_blas.cmake
@@ -0,0 +1,27 @@
+# Check for lapack and blas
+# Look for mkl libraries linked to numpy first and use those if available
+find_package(NumpyMKL)
+if(NumpyMKL_FOUND)
+    if(NumpyMKL_PREFIXES)
+        list(INSERT CMAKE_PREFIX_PATH 0 ${NumpyMKL_PREFIXES})
+        add_to_rpath(${NumpyMKL_LIBRARIES})
+    endif()
+    set(BLA_VENDOR ${NumpyMKL_VENDOR})
+    list(INSERT CMAKE_LIBRARY_PATH 0 ${NumpyMKL_LIBRARY_DIRS})
+    set(BLAS_LIBRARIES ${NumpyMKL_LIBRARIES})
+    set(LAPACK_LIBRARIES ${NumpyMKL_LIBRARIES})
+    set(LAPACK_FOUND TRUE)
+endif()
+# Then look for other possible blas libraries. The last few lines should ensure
+# we are using the numpy MKLs, if found.
+set(BLAS_INCLUDE_DIR_OPTIONAL TRUE)
+find_package(CBLAS REQUIRED)
+# Check if threading exists in library
+include(BlasThreads)
+if(NOT NumpyMKL_FOUND)
+    # Assume mkls have Lapack. Not sure this is true... but can't use numpy's mkl otherwise.
+    find_package(LAPACK REQUIRED)
+endif()
+if(NOT LAPACK_INCLUDE_DIR)
+    find_path(LAPACK_INCLUDE_DIR clapack.h)
+endif()
