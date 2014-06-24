bempp/bempp-legacy

commit 060d328f5aef8b18477adb245239348756b11cb9

Put armadillo flags in separate cached variable

cmake/check_armadillo_linkage.cmake
@@ -4,14 +4,20 @@ if(NOT ARMADILLO_INCLUDE_DIR)
 endif()
 include(CheckSymbolExists)
 set(CMAKE_REQUIRED_INCLUDE ${ARMADILLO_INCLUDE_DIR})
-check_symbol_exists(ARMA_USE_LAPACK "${ARMADILLO_INCLUDE_DIR}/armadillo" has_lapack)
-check_symbol_exists(ARMA_USE_BLAS "${ARMADILLO_INCLUDE_DIR}/armadillo" has_blas)
+check_symbol_exists(ARMA_USE_LAPACK
+    "${ARMADILLO_INCLUDE_DIR}/armadillo" has_lapack)
+check_symbol_exists(ARMA_USE_BLAS
+    "${ARMADILLO_INCLUDE_DIR}/armadillo" has_blas)
+set(ARMADILLO_CXX_FLAGS)
 if(NOT has_lapack OR NOT has_blas)
-    add_definitions(-DARMA_DONT_USE_WRAPPER)
+    set(ARMADILLO_CXX_FLAGS "${ARMADILLO_CXX_FLAGS} -DARMA_DONT_USE_WRAPPER")
 endif()
 if(NOT has_lapack)
-    add_definitions(-DARMA_USE_LAPACK)
+    set(ARMADILLO_CXX_FLAGS "${ARMADILLO_CXX_FLAGS} -DARMA_USE_LAPACK")
 endif()
 if(NOT has_blas)
-    add_definitions(-DARMA_USE_BLAS)
+    set(ARMADILLO_CXX_FLAGS "${ARMADILLO_CXX_FLAGS} -DARMA_USE_BLAS")
+endif()
+if(NOT "${ARMDILLO_CXX_FLAGS}" STREQUAL "")
+    add_definitions(${ARMADILLO_CXX_FLAGS})
 endif()

commit 9f7b0f828317f87b7b1009b6536f5201a9b8c743

:bug: armadillo cxx flags were not being set
Introduced in 060d328f5aef8b18477adb245239348756b11cb9 when putting armadillo
flags in setparate cached variables.

cmake/check_armadillo_linkage.cmake
@@ -18,6 +18,6 @@ endif()
 if(NOT has_blas)
     set(ARMADILLO_CXX_FLAGS "${ARMADILLO_CXX_FLAGS} -DARMA_USE_BLAS")
 endif()
-if(NOT "${ARMDILLO_CXX_FLAGS}" STREQUAL "")
+if(NOT "${ARMADILLO_CXX_FLAGS}" STREQUAL "")
     add_definitions(${ARMADILLO_CXX_FLAGS})
 endif()