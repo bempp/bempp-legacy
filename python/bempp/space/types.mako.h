fdc20423ecec3378240cba5899f4de120d45e71d
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-09-02 11:15:40 +0100

    Cleaned up cython typedefs header
    The file defines typedefs to avoid overly nested templates cython can't handle.
    We only need to declare explicit complex types at this juncture.

--- python/bempp/space/types.mako.h
+++ python/bempp/space/types.mako.h
@@ -1,17 +1,15 @@
+<%
+from space import dtypes
+%>\
 #ifndef BEMPP_PYTHON_SPACE_TYPES_H
 #define BEMPP_PYTHON_SPACE_TYPES_H
 
 #include<complex>
-#include "bempp/common/shared_ptr.hpp"
-#include "bempp/lib/grid.hpp"
-#include "bempp/space/space.hpp"
 
-<%
-from space import dtypes
-%>
 namespace {
 % for ctype in dtypes.itervalues():
 %     if 'complex'  in ctype:
+    //! Typedef to avoid deep template nesting cython can't handle
     typedef std::complex<${ctype[8:]}> ${ctype};
 %     endif
 % endfor
