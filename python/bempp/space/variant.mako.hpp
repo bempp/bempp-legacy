1582f124276500aa088ed48685b4e4b46daeca65
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-09-28 17:26:30 +0100

    Spaces are wrapped via a boost::variant
    This approach seems to be somewhat simpler. It should also make it easier to
    wrap shared_ptr[T] and shared_ptr[const T] instances without sacrificing const
    correctness.
    
    Added some functionality to Space wrappers to showcase new variant
    implementation.

---
+++ python/bempp/space/variant.mako.hpp
@@ -0,0 +1,84 @@
+<% from space import dtypes, spaces %>
+#ifndef BEMPP_PYTHON_SPACE_VARIANT_HPP
+#define BEMPP_PYTHON_SPACE_VARIANT_HPP
+
+#include "bempp/common/shared_ptr.hpp"
+#include "types.h"
+#include <boost/variant.hpp>
+#include <type_traits>
+#include <type_traits>
+
+namespace Bempp {
+
+//! A shared pointer that can hold const and non-const versions
+class SpaceVariants {
+    typedef boost::variant<
+% for ctype in dtypes.values():
+        shared_ptr< Space<${ctype}> >,
+        shared_ptr< const Space<${ctype}> > ${',' if not loop.last else ''}
+% endfor
+    > t_variant;
+
+    struct GetGrid : public boost::static_visitor< shared_ptr<Grid const> > {
+        template<class T> shared_ptr<Grid const>
+            operator()( shared_ptr<T> const &_in) const { return _in->grid(); }
+    };
+
+    struct IsCompatible : public boost::static_visitor<bool> {
+        template<class T0, class T1>
+            typename std::enable_if<
+                std::is_same<
+                    typename std::remove_const<T0>::type,
+                    typename std::remove_const<T1>::type
+                > :: value, bool
+            > :: type operator()(shared_ptr<T0> const &_this,
+                shared_ptr<T1> const &_that) const {
+            return _this->spaceIsCompatible(*_that);
+        }
+        template<class T0, class T1>
+            typename std::enable_if<
+                not std::is_same<
+                    typename std::remove_const<T0>::type,
+                    typename std::remove_const<T1>::type
+                > :: value, bool
+            > :: type operator()(shared_ptr<T0> const &_this,
+                shared_ptr<T1> const &_that) const {
+            return false;
+        }
+    };
+
+    struct DType: public boost::static_visitor<std::string> {
+% for pyname, ctype in dtypes.iteritems():
+        std::string operator()(
+                shared_ptr<Space<${ctype}> const> const &_in) const {
+            return "${pyname}";
+        }
+% endfor
+    };
+
+    public:
+        SpaceVariants() {}
+
+        template<class T>
+            void set(shared_ptr<T> const &_in) { space_ = _in; }
+        template<class T>
+            void set(shared_ptr<T const> const &_in) { space_ = _in; }
+
+        std::string dtype() const {
+            return boost::apply_visitor(SpaceVariants::DType(), space_);
+        }
+
+        shared_ptr<Grid const> grid() const {
+            return boost::apply_visitor(SpaceVariants::GetGrid(), space_);
+        }
+
+        bool isCompatible(SpaceVariants const &_other) const {
+            return boost::apply_visitor(SpaceVariants::IsCompatible(),
+                    space_, _other.space_);
+        }
+    private:
+        t_variant space_;
+};
+
+}
+#endif
