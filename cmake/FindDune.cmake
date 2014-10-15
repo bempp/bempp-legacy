bempp/bempp-legacy

commit d985eb77706229a65e927f8f27df0abaae76654b

Dune to build from pre-installs as well as lookup
Closes #169

Also makes dependency on external libraries more explicit in exported package.

cmake/FindDune.cmake
@@ -10,7 +10,8 @@ if(Dune_FIND_QUIETLY)
     list(APPEND arguments QUIET)
     set(quietly QUIET)
 endif()
-find_package(dune-common ${arguments} ${quietly})
+find_package(dune-common ${arguments} ${quietly}
+    HINTS ${dune_PREFIX} $ENV${dune_PREFIX})
 if(NOT dune-common_FOUND)
     return()
 endif()
@@ -22,7 +23,8 @@ list(REMOVE_ITEM components foamgrid)
 
 unset(required_not_found)
 foreach(component ${components})
-    find_package(dune-${component} ${quietly})
+    find_package(dune-${component} ${quietly}
+        HINTS ${dune_PREFIX} $ENV${dune_PREFIX})
     if(Dune_FIND_REQUIRED_${component} AND NOT dune-${component}_FOUND)
         list(APPEND required_not_found ${component})
     endif()

CMakeLists.txt
@@ -50,6 +50,8 @@ add_subdirectory(lib)
 add_subdirectory(python)
 add_subdirectory(examples)
 add_subdirectory(meshes)
+# Installs FindX.cmake files for packages depending on cmake
+add_subdirectory(cmake)
 
 # Uninstall target
 configure_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake/uninstall.in.cmake"

cmake/BemppConfig.in.cmake
@@ -7,6 +7,7 @@ if(NOT TARGET libbempp AND NOT Bempp_BINARY_DIR)
     include("${Bempp_CMAKE_DIR}/BemppTargets.cmake")
 endif()
 
+list(APPEND CMAKE_MODULE_PATH "@BEMPP_CMAKE_MODULE_PATH@")
 set(BEMPP_LIBRARY libbempp)
 set(BEMPP_INCLUDE_DIRS @ALL_INCLUDE_DIRS@)
 set(BEMPP_DIRICHLET_TUTOTIAL tutorial_dirichlet)
@@ -14,12 +15,17 @@ set(BEMPP_PYTHON_INCLUDE_DIRS @PYTHON_INCLUDE_DIRS@ @NUMPY_INCLUDE_DIRS@)
 set(BEMPP_CXX_FLAGS "@CXX11_FLAGS@ @BLAS_CMAKE_C_FLAGS@ @ARMADILLO_CXX_FLAGS@")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${BEMPP_CXX_FLAGS}")
 if(NOT "@BEMPP_PREFIX_PATH@" STREQUAL "" AND EXISTS "@BEMPP_PREFIX_PATH@")
-    # Adds other libraries, say Trilinos
     list(APPEND CMAKE_PREFIX_PATH @BEMPP_PREFIX_PATH@)
-    if(EXISTS "@BEMPP_PREFIX_PATH@/lib")
-        link_directories("@BEMPP_PREFIX_PATH@/lib")
-    endif()
-    if(EXISTS "@BEMPP_PREFIX_PATH@/lib64")
-        link_directories("@BEMPP_PREFIX_PATH@/lib64")
-    endif()
 endif()
+if(NOT "@CMAKE_PREFIX_PATH@" STREQUAL "")
+    list(APPEND CMAKE_PREFIX_PATH @CMAKE_PREFIX_PATH@)
+endif()
+
+find_package(Boost 1.55 REQUIRED)
+find_package(Armadillo REQUIRED)
+find_package(TBB REQUIRED)
+find_package(Dune REQUIRED COMPONENTS geometry grid localfunctions)
+if(@WITH_ALUGRID@)
+    find_package(ALUGrid)
+endif()
+find_package(Trilinos)

cmake/BemppExport.cmake
@@ -9,6 +9,7 @@ endif()
 
 # First in binary dir
 set(ALL_INCLUDE_DIRS "${BEMPP_INCLUDE_DIRS}")
+set(BEMPP_CMAKE_MODULE_PATH "${PROJECT_SOURCE_DIR}/cmake")
 get_filename_component(BEMPP_PREFIX_PATH "${EXTERNAL_ROOT}" ABSOLUTE)
 configure_File(cmake/BemppConfig.in.cmake
     "${PROJECT_BINARY_DIR}/BemppConfig.cmake" @ONLY
@@ -22,6 +23,7 @@ set(share_path "${SHARE_INSTALL_PATH}/cmake/Bempp")
 if(NOT IS_ABSOLUTE "${share_path}")
     set(share_path "${CMAKE_INSTALL_PREFIX}/${share_path}")
 endif()
+set(BEMPP_CMAKE_MODULE_PATH "${share_path}/cmake")
 set(include_path "${INCLUDE_INSTALL_PATH}/Bempp")
 if(NOT IS_ABSOLUTE "${include_path}")
     set(include_path "${CMAKE_INSTALL_PREFIX}/${include_path}")

cmake/BemppFindDependencies.cmake
@@ -41,7 +41,8 @@ lookup_package(Armadillo REQUIRED ARGUMENTS TIMEOUT 60)
 # ARMA_DONT_USE_WRAPPER means we don't need to include armadillo library
 add_definitions(-DARMA_DONT_USE_WRAPPER)
 lookup_package(TBB REQUIRED)
-lookup_package(Dune REQUIRED COMPONENTS geometry grid localfunctions foamgrid)
+lookup_package(Dune REQUIRED COMPONENTS geometry grid localfunctions)
+include("${PROJECT_SOURCE_DIR}/cmake/dune/local.cmake")
 
 # Using cmake_policy does not seem to work here.
 set(CMAKE_POLICY_DEFAULT_CMP0012 NEW CACHE STRING "Avoids anoying messages")

cmake/CMakeLists.txt
@@ -1,0 +1,7 @@
+# Needed when importing a project
+install(FILES
+    FindALUGrid.cmake
+    FindDune.cmake
+    FindTBB.cmake
+    DESTINATION ${SHARE_INSTALL_PATH}/cmake/Bempp/cmake
+)

cmake/Dune/local.cmake
@@ -1,0 +1,86 @@
+unset(depends)
+foreach(component common geometry grid localfunctions)
+    if(TARGET dune-${component})
+        list(APPEND depends dune-${component})
+    endif()
+endforeach()
+ExternalProject_Add(dune-foamgrid
+    DEPENDS ${depends}
+    PREFIX ${EXTERNAL_ROOT}
+    URL ${PROJECT_SOURCE_DIR}/contrib/dune/dune-foamgrid
+    PATCH_COMMAND
+       ${CMAKE_COMMAND} -E copy_if_different
+                        ${CMAKE_CURRENT_LIST_DIR}/foamgrid-install.cmake
+                        ${EXTERNAL_ROOT}/src/dune-foamgrid/CMakeLists.txt
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_ROOT}
+)
+
+# This file helps to create a fake dune project
+# It answers the question posed by the duneproject script, including the subsidiary
+# "this directory already exists..."
+file(
+  WRITE ${EXTERNAL_ROOT}/src/bempp.dune.input
+  "dune-bempp
+dune-common dune-geometry dune-grid dune-localfunctions
+1
+me@me
+y
+y
+"
+)
+if(NOT TARGET Dune)
+    include(PassonVariables)
+    passon_variables(Dune
+        FILENAME "${EXTERNAL_ROOT}/src/DuneVariables.cmake"
+        PATTERNS
+            "CMAKE_[^_]*_R?PATH" "CMAKE_C_.*" "CMAKE_CXX_.*"
+            "BLAS_.*" "LAPACK_.*"
+        ALSOADD
+            "\nset(CMAKE_INSTALL_PREFIX \"${EXTERNAL_ROOT}\" CACHE STRING \"\")\n"
+            "set(CMAKE_LIBRARY_PATH ${library_dirs} \"${EXTERNAL_ROOT}/lib\"\n"
+            "    CACHE PATH \"\" FORCE)\n"
+            "set(CMAKE_INSTALL_RPATH ${rpath_dirs} CACHE INTERNAL \"\")\n"
+            "set(CMAKE_PROGRAM_PATH \"${EXTERNAL_ROOT}/bin\" CACHE PATH \"\")\n"
+            "set(BUILD_SHARED_LIBS TRUE CACHE BOOL \"\" FORCE)\n"
+            "set(CMAKE_BUILD_TYPE Release CACHE INTERNAL \"\" FORCE)\n"
+            "set(CMAKE_DISABLE_FIND_PACKAGE_HDF5 TRUE CACHE BOOL \"\" FORCE)\n"
+            "set(CMAKE_DISABLE_FIND_PACKAGE_MPI TRUE CACHE BOOL \"\" FORCE)\n"
+            "set(CMAKE_DISABLE_FIND_PACKAGE_Doxygen TRUE CACHE BOOL \"\" FORCE)\n"
+    )
+endif()
+
+# Create fake dune project, with the sole goal of generating a config.h file!
+# First, generate a new project
+ExternalProject_Add(
+    dune-bempp
+    DEPENDS dune-foamgrid
+    PREFIX ${EXTERNAL_ROOT}
+    DOWNLOAD_COMMAND ""
+    CMAKE_ARGS -C "${EXTERNAL_ROOT}/src/DuneVariables.cmake"
+    LOG_DOWNLOAD OFF LOG_CONFIGURE ON LOG_BUILD ON
+    INSTALL_COMMAND
+        ${CMAKE_COMMAND} -E copy_if_different
+               ${EXTERNAL_ROOT}/src/dune-bempp-build/config.h
+               ${EXTERNAL_ROOT}/include/dune_config.h
+    ${build_args}
+)
+find_program(DuneProject_PROGRAM duneproject PATHS
+    "${EXTERNAL_ROOT}/src/dune-common/bin")
+ExternalProject_Add_Step(dune-bempp
+    CREATE_PROJECT
+    COMMAND ${DuneProject_PROGRAM} < bempp.dune.input
+    WORKING_DIRECTORY ${EXTERNAL_ROOT}/src
+    COMMENT Creating fake dune-bempp project
+    DEPENDERS configure
+)
+set_target_properties(
+    dune-foamgrid dune-bempp
+    PROPERTIES EXCLUDE_FROM_ALL TRUE
+)
+
+if(NOT TARGET Dune)
+    add_custom_target(Dune ALL DEPENDS dune-bempp dune-foamgrid)
+    add_dependencies(lookup_dependencies Dune)
+else()
+    add_dependencies(Dune dune-foamgrid dune-bempp)
+endif()

cmake/Dune/lookup.cmake
@@ -116,7 +116,7 @@ ExternalProject_Add(dune-grid
 _get_arguments(localfunctions)
 ExternalProject_Add(dune-localfunctions
     PREFIX ${EXTERNAL_ROOT}
-    DEPENDS dune-geometry dune-common
+    DEPENDS dune-geometry dune-common dune-grid
     ${localfunctions_ARGUMENTS}
     PATCH_COMMAND
         ${CMAKE_COMMAND} -DROOT=${EXTERNAL_ROOT}/src/dune-localfunctions
@@ -125,71 +125,21 @@ ExternalProject_Add(dune-localfunctions
     LOG_DOWNLOAD ON LOG_CONFIGURE ON LOG_BUILD ON
 )
 
-ExternalProject_Add(dune-foamgrid
-    DEPENDS dune-grid dune-geometry dune-common
-    PREFIX ${EXTERNAL_ROOT}
-    URL ${PROJECT_SOURCE_DIR}/contrib/dune/dune-foamgrid
-    PATCH_COMMAND
-       ${CMAKE_COMMAND} -E copy_if_different
-                        ${CURRENT_LOOKUP_DIRECTORY}/foamgrid-install.cmake
-                        ${EXTERNAL_ROOT}/src/dune-foamgrid/CMakeLists.txt
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNAL_ROOT}
-)
-
-# This file helps to create a fake dune project
-# It answers the question posed by the duneproject script, including the subsidiary
-# "this directory already exists..."
-file(
-  WRITE ${EXTERNAL_ROOT}/src/bempp.dune.input
-  "dune-bempp
-dune-common dune-geometry dune-grid dune-localfunctions
-1
-me@me
-y
-y
-"
-)
-
-# Create fake dune project, with the sole goal of generating a config.h file!
-# First, generate a new project
-ExternalProject_Add(
-    dune-bempp
-    DEPENDS dune-foamgrid dune-grid dune-geometry dune-common dune-localfunctions
-    PREFIX ${EXTERNAL_ROOT}
-    DOWNLOAD_COMMAND ""
-    CMAKE_ARGS -C "${EXTERNAL_ROOT}/src/DuneVariables.cmake"
-    LOG_DOWNLOAD OFF LOG_CONFIGURE ON LOG_BUILD ON
-    INSTALL_COMMAND
-        ${CMAKE_COMMAND} -E copy_if_different
-               ${EXTERNAL_ROOT}/src/dune-bempp-build/config.h
-               ${EXTERNAL_ROOT}/include/dune_config.h
-    ${build_args}
-)
-ExternalProject_Add_Step(dune-bempp
-    CREATE_PROJECT
-    COMMAND dune-common/bin/duneproject < bempp.dune.input
-    WORKING_DIRECTORY ${EXTERNAL_ROOT}/src
-    COMMENT Creating fake dune-bempp project
-    DEPENDERS configure
-)
-
 # Creates a single target for Dune and remove subcomponents from ALL
 add_custom_target(Dune ALL
     DEPENDS
-        dune-foamgrid dune-grid dune-geometry dune-common dune-localfunctions
-        dune-bempp
+       dune-grid dune-geometry dune-common dune-localfunctions
 )
 set_target_properties(
-    dune-foamgrid dune-grid dune-geometry dune-common dune-localfunctions
-    dune-bempp
+    dune-grid dune-geometry dune-common dune-localfunctions
     PROPERTIES EXCLUDE_FROM_ALL TRUE
 )
 
 
 # Rerun cmake to capture new dune install
-add_recursive_cmake_step(dune-bempp
+add_recursive_cmake_step(dune-localfunctions
     PACKAGE_NAME Dune
     FOUND_VAR Dune_FOUND
     DEPENDEES install
 )
-add_dependencies(lookup_dependencies dune-bempp)
+add_dependencies(lookup_dependencies Dune)

commit 29b48e2c4e93533caaa7d16d9cd612db6faed339

Syntax error introduced in d985eb77706229a65e927f8
Was setting correct hint for path to dune prefix from environment variable.

cmake/FindDune.cmake
@@ -11,7 +11,7 @@ if(Dune_FIND_QUIETLY)
     set(quietly QUIET)
 endif()
 find_package(dune-common ${arguments} ${quietly}
-    HINTS ${dune_PREFIX} $ENV${dune_PREFIX})
+    HINTS ${dune_PREFIX} $ENV{dune_PREFIX})
 if(NOT dune-common_FOUND)
     return()
 endif()
@@ -24,7 +24,7 @@ list(REMOVE_ITEM components foamgrid)
 unset(required_not_found)
 foreach(component ${components})
     find_package(dune-${component} ${quietly}
-        HINTS ${dune_PREFIX} $ENV${dune_PREFIX})
+        HINTS ${dune_PREFIX} $ENV{dune_PREFIX})
     if(Dune_FIND_REQUIRED_${component} AND NOT dune-${component}_FOUND)
         list(APPEND required_not_found ${component})
     endif()