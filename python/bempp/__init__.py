92cea90d832bdb988537b3d3d88ca7632a42cf79
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-01-07 10:54:05 +0000

    Add test function to run all pytests

--- python/bempp/__init__.py
+++ python/bempp/__init__.py
@@ -1,1 +1,7 @@
 """ Boundary Element Method package BEM++ """
+
+def test():
+    """ Runs BEM++ python unit tests """
+    from py.test import main
+    from os.path import dirname
+    main(dirname(__file__))
