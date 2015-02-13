98226d4d0d525c101fa547a2b2088731d511cb7e
Timo Betcke <timo.betcke@gmail.com>
2015-02-13 22:53:56 +0000

    Added Eigen3

---
+++ cmake/FindEigen3.cmake
@@ -0,0 +1,22 @@
+# Try to find Eigen3
+# Will define
+#
+# Eigen3_FOUND
+# Eigen3_INCLUDE_DIR
+
+if(NOT Eigen3_INCLUDE_DIR)
+    set(Eigen3_INCLUDE_DIR ${PROJECT_BINARY_DIR}/external/include)
+endif()
+
+find_path(Eigen3_INCLUDE_DIR Core
+    HINTS ${Eigen3_INCLUDE_DIR}
+    PATH_SUFFIXES Eigen
+    DOC "Eigen3 Directory"
+    )
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(
+  Eigen3 
+  DEFAULT_MSG
+  Eigen3_INCLUDE_DIR
+)
