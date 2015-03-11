6d6eeb51f9871a86c77efc83eafb51738f165f64
Timo Betcke <timo.betcke@gmail.com>
2015-03-11 00:04:52 +0000

    Added BlockedLinearOperator

--- python/bempp/fenics_interface/__init__.py
+++ python/bempp/fenics_interface/__init__.py
@@ -3,4 +3,5 @@ try:
 except:
     print("Cannot find dolfin. Please check your FEniCS installation.")
 
-
+from .fenics_operator import FenicsOperator
+from .coupling import boundary_grid_from_fenics_mesh,fenics_to_bempp_trace_data,fenics_space_info
--- python/bempp/utils/__init__.py
+++ python/bempp/utils/__init__.py
@@ -1,2 +1,4 @@
 from .data_types import combined_type
+from .blocked_linear_operator import BlockedDiscreteLinearOperator, BlockedLinearOperator
+
 
