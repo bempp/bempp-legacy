7c55bd1cc957d40146728de58f49d68c116bf9c9
Timo Betcke <timo.betcke@gmail.com>
2015-03-11 13:29:04 +0000

    Added unit test for FEniCS P1 interface

--- python/bempp/__init__.py
+++ python/bempp/__init__.py
@@ -10,6 +10,13 @@ __all__=['GmshInterface',
          'BlockedBoundaryOperator',
          'export']
 
+# This imports dolfin at the same time as bempp if available to avoid delays
+# at later imports of dolfin
+try:
+    import dolfin as _dolfin
+except:
+    pass
+
 from bempp.grid import Grid,grid_from_sphere,grid_from_element_data, structured_grid
 from bempp.assembly import GridFunction
 from bempp.assembly import BlockedBoundaryOperator
