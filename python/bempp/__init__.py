643ceba4ecdc270a9b7307299f8513148721c1f3
Timo Betcke <timo.betcke@gmail.com>
2015-04-08 18:55:12 +0200

    Python spaces interface works

--- python/bempp/__init__.py
+++ python/bempp/__init__.py
@@ -12,7 +12,8 @@
 
 __all__=['grid_from_sphere',
          'grid_from_element_data',
-         'structured_grid']
+         'structured_grid',
+         'function_space']
 
 # This imports dolfin at the same time as bempp if available to avoid delays
 # at later imports of dolfin
@@ -24,7 +25,7 @@ except:
 from bempp.grid import Grid,grid_from_sphere,grid_from_element_data, structured_grid
 #from bempp.assembly import GridFunction
 #from bempp.assembly import BlockedBoundaryOperator
-#from bempp.space import function_space
+from bempp.space import function_space
 #from bempp.file_interfaces.gmsh import GmshInterface
 #from bempp.common import global_parameters
 #from bempp.file_interfaces import export
