691f47421a7be7033c891eb213f9fdf753059d52
MScroggs <matthew.w.scroggs@gmail.com>
2015-07-10 10:44:14 +0100

    added blocked cg

--- python/bempp/linalg/blocked.py
+++ python/bempp/linalg/blocked.py
@@ -15,3 +15,19 @@ def gmres(A, b, tol=1E-5, restart=None, maxiter=None, M=None, callback=None):
         output.append(soln[prev:curr])
 
     return output,info
+
+
+def cg(A, b, tol=1E-5, maxiter=None, M=None, callback=None):
+
+    if not isinstance(A,BlockedDiscreteLinearOperator):
+        raise ValueError("A must be of type BlockedDiscreteLinearOperator")
+
+    soln,info = scipy.sparse.linalg.cg(A, b, tol=1E-5, restart=restart, maxiter=maxiter, M=M, callback=callback)
+    output = []
+    curr = 0
+    for column_size in A._cols:
+        prev = curr
+        curr += column_size
+        output.append(soln[prev:curr])
+
+    return output,info
