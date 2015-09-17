d30edf83f99dc3c1fc7c17b407dc12109c94ab30
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-09-17 15:51:11 +0100

    Refactor tests

--- python/tests/bempp/utils/test_pickle_global_options.py
+++ python/tests/bempp/utils/test_pickle_global_options.py
@@ -7,52 +7,56 @@ class TestPickleGlobalOptions(object):
 
         serialized = pickle.dumps(bempp.global_parameters)
         restored = pickle.loads(serialized)
-        assert bempp.global_parameters.assembly.boundary_operator_assembly_type \
+        orig = bempp.global_parameters
+        self.__compare_options(orig, restored)
+
+    def __compare_options(self, orig, restored):
+        assert orig.assembly.boundary_operator_assembly_type \
                            == restored.assembly.boundary_operator_assembly_type
-        assert bempp.global_parameters.assembly.potential_operator_assembly_type \
+        assert orig.assembly.potential_operator_assembly_type \
                            == restored.assembly.potential_operator_assembly_type
-        assert bempp.global_parameters.assembly.enable_singular_integral_caching \
+        assert orig.assembly.enable_singular_integral_caching \
                            == restored.assembly.enable_singular_integral_caching
 
-        assert bempp.global_parameters.quadrature.double_singular \
+        assert orig.quadrature.double_singular \
                            == restored.quadrature.double_singular
 
-        assert bempp.global_parameters.quadrature.near.max_rel_dist \
+        assert orig.quadrature.near.max_rel_dist \
                            == restored.quadrature.near.max_rel_dist
-        assert bempp.global_parameters.quadrature.near.single_order \
+        assert orig.quadrature.near.single_order \
                            == restored.quadrature.near.single_order
-        assert bempp.global_parameters.quadrature.near.double_order \
+        assert orig.quadrature.near.double_order \
                            == restored.quadrature.near.double_order
 
-        assert bempp.global_parameters.quadrature.medium.max_rel_dist \
+        assert orig.quadrature.medium.max_rel_dist \
                            == restored.quadrature.medium.max_rel_dist
-        assert bempp.global_parameters.quadrature.medium.single_order \
+        assert orig.quadrature.medium.single_order \
                            == restored.quadrature.medium.single_order
-        assert bempp.global_parameters.quadrature.medium.double_order \
+        assert orig.quadrature.medium.double_order \
                            == restored.quadrature.medium.double_order
 
-        assert bempp.global_parameters.quadrature.far.single_order \
+        assert orig.quadrature.far.single_order \
                            == restored.quadrature.far.single_order
-        assert bempp.global_parameters.quadrature.far.double_order \
+        assert orig.quadrature.far.double_order \
                            == restored.quadrature.far.double_order
 
-        assert bempp.global_parameters.hmat.assembly_mode \
+        assert orig.hmat.assembly_mode \
                            == restored.hmat.assembly_mode
-        assert bempp.global_parameters.hmat.min_block_size \
+        assert orig.hmat.min_block_size \
                            == restored.hmat.min_block_size
-        assert bempp.global_parameters.hmat.max_block_size \
+        assert orig.hmat.max_block_size \
                            == restored.hmat.max_block_size
-        assert bempp.global_parameters.hmat.eta \
+        assert orig.hmat.eta \
                            == restored.hmat.eta
-        assert bempp.global_parameters.hmat.eps \
+        assert orig.hmat.eps \
                            == restored.hmat.eps
-        assert bempp.global_parameters.hmat.max_rank \
+        assert orig.hmat.max_rank \
                            == restored.hmat.max_rank
-        assert bempp.global_parameters.hmat.compression_algorithm \
+        assert orig.hmat.compression_algorithm \
                            == restored.hmat.compression_algorithm
-        assert bempp.global_parameters.hmat.coarsening \
+        assert orig.hmat.coarsening \
                            == restored.hmat.coarsening
-        assert bempp.global_parameters.hmat.coarseningAccuracy \
+        assert orig.hmat.coarseningAccuracy \
                            == restored.hmat.coarseningAccuracy
-        assert bempp.global_parameters.hmat.mat_vec_parallel_levels \
+        assert orig.hmat.mat_vec_parallel_levels \
                            == restored.hmat.mat_vec_parallel_levels
