2bff533cc6b0a10a3b72a03d24fe0db9405cff11
MScroggs <matthew.w.scroggs@gmail.com>
2015-09-23 14:08:05 +0100

    Allowed solvers to be used on arrays and wrapped barycentric_space

--- python/bempp/assembly/__init__.py
+++ python/bempp/assembly/__init__.py
@@ -7,6 +7,7 @@ from .discrete_boundary_operator import InverseSparseDiscreteBoundaryOperator
 from .discrete_boundary_operator import as_matrix
 from .boundary_operator import LocalBoundaryOperator
 from .boundary_operator import ElementaryBoundaryOperator
+from .boundary_operator import BoundaryOperator
 from .boundary_operator import ZeroBoundaryOperator
 from .grid_function import GridFunction
 from .assembler import assemble_dense_block
