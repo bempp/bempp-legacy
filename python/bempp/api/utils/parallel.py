d94f3af4115cf3433842ca375eaf86c717855fee
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-10-02 17:34:26 +0100

    WIP Parallel work

---
+++ python/bempp/api/utils/parallel.py
@@ -0,0 +1,50 @@
+"Some helper functions for parallel calculations"
+
+
+def calculateblocks(nrows, ncols, numengines):
+    """"Given the x and y size of an array split it in upto numengine chunks"""
+
+    # we have ncoleng engines along the xaxis and nroweng engines
+    # along the y axis with ncoleng *nroweng > numengines
+    # nchunks = nroweng*ncoleng
+    ncoleng, nroweng = _calculateenginesplit(nrows, ncols, numengines)
+    chunksizerows = nrows // nroweng
+    chunksizecols = ncols // ncoleng
+    blocks = list()
+    for i in range(nroweng):
+        for j in range(ncoleng):
+            colsstart = chunksizecols*i
+            if i == ncols-1:
+                colsend = ncols
+            else:
+                colsend = chunksizecols*(i+1)
+            rowsstart = chunksizerows*j
+            if j == nrows-1:
+                rowsend = nrows
+            else:
+                rowsend = chunksizerows*(j+1)
+            blocks.append(((colsstart, colsend), (rowsstart, rowsend)))
+    return blocks, nroweng, ncoleng
+
+
+def _calculateenginesplit(nrows, ncols, numengines):
+    "Private helper function for calculateblocks"
+    import math
+    aspectratio = nrows//ncols
+    if aspectratio > 1.1:
+        raise NotImplementedError
+    elif aspectratio < 0.9:
+        raise NotImplementedError
+    else:
+        nroweng = int(math.floor(numengines**(1./2)))
+        ncoleng = nroweng
+    return (ncoleng, nroweng)
+
+
+def gatherresults(view, resultname, nrows, ncols):
+    """Gather results from engines"""
+    import numpy as np
+    partial = [np.concatenate(view[resultname][i*ncols:(i+1)*ncols],
+                              axis=1) for i in range(nrows)]
+    result = np.concatenate(partial, axis=0)
+    return result
---
+++ python/bempp/api/utils/test/test_parallel.py
@@ -0,0 +1,34 @@
+"""Test cases for parallel utils"""
+from unittest import TestCase
+import bempp
+import numpy as np
+
+
+class TestParallel(TestCase):
+    """Test cases for parallel utils"""
+
+    def test_calculateblocks(self):
+        """Test that calulate blocks works in a simple case"""
+        from bempp.api.utils.parallel import calculateblocks
+        chunks, nrowseng, ncolseng = calculateblocks(100, 100, 4)
+
+        assert nrowseng == 2
+        assert nrowseng == 2
+        low = (0, 50)
+        high = (50, 100)
+
+        expectedchunks = [(low, low),
+                          (low, high),
+                          (high, low),
+                          (high, high)]
+        np.testing.assert_array_equal(chunks, expectedchunks)
+
+
+    def test_gatherresults():
+        pass
+
+
+if __name__ == "__main__":
+    from unittest import main
+
+    main()
