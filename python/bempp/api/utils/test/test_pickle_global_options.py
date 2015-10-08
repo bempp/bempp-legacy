fb059c7266d1da5eab7a225ac0f68d70a1947e56
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-10-08 15:00:21 +0100

    fix and simplify tests

--- python/bempp/api/utils/test/test_pickle_global_options.py
+++ python/bempp/api/utils/test/test_pickle_global_options.py
@@ -1,58 +1,48 @@
+from unittest import TestCase
 import bempp
+import bempp.api
 import pickle
 
-class TestPickleGlobalOptions(object):
+
+class TestPickleGlobalOptions(TestCase):
 
     def test_pickle_global_options(self):
+        def _iterateorcompare(optionsdict, original, restored):
+            for key in optionsdict.keys():
+                suboriginal = getattr(original, key)
+                subrestored = getattr(restored, key)
+                suboptionsdict = optionsdict[key]
+                for element in suboptionsdict:
+                    if isinstance(element, str):
+                        originalval = getattr(suboriginal, element)
+                        restoredval = getattr(subrestored, element)
+                        assert originalval == restoredval
+                        print("testing " + str(originalval))
+                    else:
+                        _iterateorcompare(element, suboriginal, subrestored)
 
-        serialized = pickle.dumps(bempp.global_parameters)
+        serialized = pickle.dumps(bempp.api.global_parameters)
+        original = bempp.api.global_parameters
         restored = pickle.loads(serialized)
-        assert bempp.global_parameters.assembly.boundary_operator_assembly_type \
-                           == restored.assembly.boundary_operator_assembly_type
-        assert bempp.global_parameters.assembly.potential_operator_assembly_type \
-                           == restored.assembly.potential_operator_assembly_type
-        assert bempp.global_parameters.assembly.enable_singular_integral_caching \
-                           == restored.assembly.enable_singular_integral_caching
+        optionsdict = {}
+        optionsdict['assembly'] = ['boundary_operator_assembly_type',
+                                   'potential_operator_assembly_type',
+                                   'enable_singular_integral_caching']
+        quadraturesuboptions = {}
+        quadraturesuboptions['far'] = ['single_order', 'double_order']
+        quadraturesuboptions['medium'] = ['max_rel_dist', 'single_order',
+                                          'double_order']
+        quadraturesuboptions['near'] = quadraturesuboptions['medium']
+        optionsdict['quadrature'] = ['double_singular',
+                                     quadraturesuboptions]
+        optionsdict['hmat'] = ['admissibility', 'coarsening',
+                               'coarseningAccuracy', 'compression_algorithm',
+                               'eps', 'eta', 'mat_vec_parallel_levels',
+                               'max_block_size', 'max_rank']
+        _iterateorcompare(optionsdict, original, restored)
 
-        assert bempp.global_parameters.quadrature.double_singular \
-                           == restored.quadrature.double_singular
 
-        assert bempp.global_parameters.quadrature.near.max_rel_dist \
-                           == restored.quadrature.near.max_rel_dist
-        assert bempp.global_parameters.quadrature.near.single_order \
-                           == restored.quadrature.near.single_order
-        assert bempp.global_parameters.quadrature.near.double_order \
-                           == restored.quadrature.near.double_order
+if __name__ == "__main__":
+    from unittest import main
 
-        assert bempp.global_parameters.quadrature.medium.max_rel_dist \
-                           == restored.quadrature.medium.max_rel_dist
-        assert bempp.global_parameters.quadrature.medium.single_order \
-                           == restored.quadrature.medium.single_order
-        assert bempp.global_parameters.quadrature.medium.double_order \
-                           == restored.quadrature.medium.double_order
-
-        assert bempp.global_parameters.quadrature.far.single_order \
-                           == restored.quadrature.far.single_order
-        assert bempp.global_parameters.quadrature.far.double_order \
-                           == restored.quadrature.far.double_order
-
-        assert bempp.global_parameters.hmat.assembly_mode \
-                           == restored.hmat.assembly_mode
-        assert bempp.global_parameters.hmat.min_block_size \
-                           == restored.hmat.min_block_size
-        assert bempp.global_parameters.hmat.max_block_size \
-                           == restored.hmat.max_block_size
-        assert bempp.global_parameters.hmat.eta \
-                           == restored.hmat.eta
-        assert bempp.global_parameters.hmat.eps \
-                           == restored.hmat.eps
-        assert bempp.global_parameters.hmat.max_rank \
-                           == restored.hmat.max_rank
-        assert bempp.global_parameters.hmat.compression_algorithm \
-                           == restored.hmat.compression_algorithm
-        assert bempp.global_parameters.hmat.coarsening \
-                           == restored.hmat.coarsening
-        assert bempp.global_parameters.hmat.coarseningAccuracy \
-                           == restored.hmat.coarseningAccuracy
-        assert bempp.global_parameters.hmat.mat_vec_parallel_levels \
-                           == restored.hmat.mat_vec_parallel_levels
+    main()
