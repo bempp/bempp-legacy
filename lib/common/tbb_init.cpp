9cf402381c623ff90d63089fbf7a354600ae00cc
Timo Betcke <timo.betcke@gmail.com>
2016-01-26 21:54:59 +0000

    Fixed bug in manual assignment of thread number

--- lib/common/tbb_init.cpp
+++ lib/common/tbb_init.cpp
@@ -4,25 +4,21 @@
 
 namespace Bempp {
 
-TbbInit::TbbInit(){
+TbbInit::TbbInit() {
 
-    unsigned int num_threads=0;
-    char* p_num_threads = std::getenv("BEMPP_NUM_THREADS");
-    if (p_num_threads!=nullptr){
-        try{
-            num_threads = boost::lexical_cast<unsigned int>(*p_num_threads);
-        }
-        catch (const boost::bad_lexical_cast&){
-        }
+  unsigned int num_threads = 0;
+  char *p_num_threads = std::getenv("BEMPP_NUM_THREADS");
+  if (p_num_threads != nullptr) {
+    try {
+      num_threads = boost::lexical_cast<unsigned int>(p_num_threads);
+    } catch (const boost::bad_lexical_cast &) {
     }
-    if (num_threads<1){
-        m_task_scheduler.reset(new tbb::task_scheduler_init());
+  }
+  if (num_threads < 1) {
+    m_task_scheduler.reset(new tbb::task_scheduler_init());
 
-    }
-    else{
-        m_task_scheduler.reset(
-                new tbb::task_scheduler_init(num_threads));
-    }
-    }
-    
+  } else {
+    m_task_scheduler.reset(new tbb::task_scheduler_init(num_threads));
+  }
+}
 }
