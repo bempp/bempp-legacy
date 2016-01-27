142425dbb4e8c6d418f9cd85e79056fad1841aa2
Timo Betcke <timo.betcke@gmail.com>
2016-01-27 22:36:36 +0000

    Fixed relative import in coupling.py

--- python/bempp/api/fenics_interface/coupling.py
+++ python/bempp/api/fenics_interface/coupling.py
@@ -21,7 +21,7 @@ def fenics_to_bempp_trace_data(fenics_space):
 
     if family=="Lagrange":
         if degree==1:
-            import p1_coupling
+            from . import p1_coupling
             return p1_coupling.p1_trace(fenics_space)
         else:
             raise NotImplementedError
