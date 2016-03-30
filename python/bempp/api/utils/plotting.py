9f70ee794daa0ab06d5264dc279d3fb8dadffd72
MScroggs <matthew.w.scroggs@gmail.com>
2016-03-30 16:09:51 +0100

    Added documentation to plotting

--- python/bempp/api/utils/plotting.py
+++ python/bempp/api/utils/plotting.py
@@ -1,6 +1,46 @@
-def plot_slice(plot_f, dolfin_function=None, x=None, y=None, z=None, n=(151,151), extent=[-5,5,-5,5],
+def plot_slice(plot_f, dolfin_function=None, x=None, y=None, z=None, n=(151,151), extent=(-5,5,-5,5),
                  cmap='coolwarm', filename=None, title=None, show=None):
-    """Plots a slice of the function plot_f at a constant x, y or z."""
+    """Plots a slice of the function plot_f at a constant x, y or z.
+
+    Parameters
+    ----------
+    plot_f : function
+        A Python function which takes one input, points, and returns
+        the value at these points.
+
+    dolfin_function : function
+        A Python function which takes one input, point, and returns the
+        value at that point, or None if the point is outside the FEM
+        domain.
+
+    x : float
+        The x-coordinate to plot a slice at.
+
+    y : float
+        The y-coordinate to plot a slice at.
+
+    z : float
+        The z-coordinate to plot a slice at.
+
+    n : tuple
+        The number of points to use on each axis.
+
+    extent : tuple
+        The bounds of the two axes.
+
+    cmap : string
+        The colour map to use on the plot.
+
+    filename : string
+        The file name to save the plot to.
+
+    title : string
+        The title of the plot.
+
+    show : bool
+        If True, the plot will be displayed. If False, it wil not.
+
+    """
     import numpy as np
     if show is None:
         show = filename==None
@@ -24,11 +64,11 @@ def plot_slice(plot_f, dolfin_function=None, x=None, y=None, z=None, n=(151,151)
         bem_x = np.full(points.shape[1], False, dtype=np.bool)
         fem_plot = []
         for i,p in enumerate(points.T):
-            try:
-                value = dolfin_function(np.array(p))
-                fem_plot.append(value)
-            except RuntimeError:
+            value = dolfin_function(np.array(p))
+            if value is None:
                 bem_x[i] = True
+            else:
+                fem_plot.append(value)
         plot_me[np.logical_not(bem_x)] = np.real(np.array(fem_plot))
         plot_me[bem_x] = np.real(plot_f(points[:,bem_x]))
 
